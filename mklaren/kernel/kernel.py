5acecb5ab53ff5b3ba4a64c9045d9f891485b7e5
mstrazar <martin.strazar@gmail.com>
2016-05-07 20:35:30 +0200

    Documentation to kernel module.

--- mklaren/kernel/kernel.py
+++ mklaren/kernel/kernel.py
@@ -1,97 +1,125 @@
+""" Methods related to calculation of kernel function values and kernel
+    matrices.
+"""
+
 import numpy as np
 import numpy.ma as ma
 from itertools import product
 import scipy.sparse as sp
 
-def linear_kernel(x1, x2):
+def linear_kernel(x, y):
         """
-        :param x1:
-            Data point(s) 1 shape(n_samples, n_features).
-        :param x2:
-            Data point(s) 2 shape(n_samples, n_features).
-        :return:
-            Linear kernel between data points shape(n_samples, n_samples).
+        The linear kernel (the usual dot product in n-dimensional space).
+
+        .. math::
+            k(\mathbf{x}, \mathbf{y}) = \mathbf{x}^T \mathbf{y}
+
+        :param x: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param y: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :return: (``numpy.ndarray``) Kernel value/matrix between data points.
+
         """
-        if isinstance(x1, int):
-            return x1 * x2
-        if sp.isspmatrix(x1):
-            return np.array(x1.dot(x2.T).todense())
+        if isinstance(x, int):
+            return x * y
+        if sp.isspmatrix(x):
+            return np.array(x.dot(y.T).todense())
         else:
-            return x1.dot(x2.T)
+            return x.dot(y.T)
 
 
-def poly_kernel(x1, x2, p=2):
+def poly_kernel(x, y, p=2, b=0):
         """
-        :param x1:
-            Data point(s) 1 shape(n_samples, n_features).
-        :param x2:
-            Data point(s) 2 shape(n_samples, n_features).
-        :param p
-            Polynomial degree.
-        :return:
-            Polynomial kernel between data points shape(n_samples, n_samples).
+        The polynomial kernel.
+
+        .. math::
+            k(\mathbf{x}, \mathbf{y}) = (b + \mathbf{x}^T \mathbf{y})^p
+
+        :param x: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param y: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param p: (``float``) Polynomial degree.
+
+        :param b: (``float``) Bias term.
+
+        :return: (``numpy.ndarray``) Kernel value/matrix between data points.
         """
-        if sp.isspmatrix(x1):
-            return np.array(x1.dot(x2.T).todense())**p
-        if not hasattr(x1, "shape"):
-            return (x1 * x2)**p
+        if sp.isspmatrix(x):
+            return np.array(x.dot(y.T).todense())**p
+        if not hasattr(x, "shape"):
+            return (x * y)**p
         else:
-            return x1.dot(x2.T)**p
+            return x.dot(y.T)**p
 
 
-def sigmoid_kernel(x1, x2, a=1, c=0):
+def sigmoid_kernel(x, y, b=1, c=0):
         """
-        :param x1:
-            Data point(s) 1 shape(n_samples, n_features).
-        :param x2:
-            Data point(s) 2 shape(n_samples, n_features).
-        :param a:
-            Scale.
-        :param c:
-            Bias term.
-        :return:
-            Sigmoid kernel between data points shape(n_samples, n_samples).
+        The sigmoid kernel.
+
+        .. math::
+            k(\mathbf{x}, \mathbf{y}) = tan(c \mathbf{x}^T \mathbf{y} + b)
+
+        :param x: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param y: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param c: (``float``) Scale.
+
+        :param b: (``float``) Bias term.
+
+        :return: (``numpy.ndarray``) Kernel value/matrix between data points.
         """
-        if sp.isspmatrix(x1) and sp.isspmatrix(x2):
-            x1 = np.array(x1.todense())
-            x2 = np.array(x2.todense())
-        if not hasattr(x1, "shape"):
-            return np.tanh(a * x1 * x2 + c)
+        if sp.isspmatrix(x) and sp.isspmatrix(y):
+            x = np.array(x.todense())
+            y = np.array(y.todense())
+        if not hasattr(x, "shape"):
+            return np.tanh(b * x * y + c)
         else:
-            return np.tanh(a * x1.dot(x2.T) + c)
+            return np.tanh(b * x.dot(y.T) + c)
 
 
-def exponential_kernel(x1, x2, sigma=2.0, gamma=None):
+def exponential_kernel(x, y, sigma=2.0, gamma=None):
     """
+    The exponential quadratic / radial basis kernel (RBF) kernel.
 
-    Also known as RBF kernel.
-        rbf_kernel
+        .. math::
+            k(\mathbf{x}, \mathbf{y}) = exp\{\dfrac{\|\mathbf{x} - \mathbf{y}\|}{\sigma^2} \}
 
-    :param x1:
-        Data point(s) 1 shape(n_samples, n_features).
-    :param x2:
-        Data point(s) 2 shape(n_samples, n_features).
-    :return:
-        Linear kernel between data points shape(n_samples, n_samples).
+        or
+
+        .. math::
+            k(\mathbf{x}, \mathbf{y}) = exp\{\gamma \|\mathbf{x} - \mathbf{y}\| \}
+
+        :param x: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param y: (``numpy.ndarray``) Data point(s) of shape ``(n_samples, n_features)`` or ``(n_features, )``.
+
+        :param sigma: (``float``) Length scale.
+
+        :param gamma: (``float``) Scale.
+
+        :return: (``numpy.ndarray``) Kernel value/matrix between data points.
     """
 
     if gamma is None:
         gamma = 1.0 / (2.0 * sigma ** 2)
 
-    if sp.isspmatrix(x1) and sp.isspmatrix(x2):
-        x1 = np.array(x1.todense())
-        x2 = np.array(x2.todense())
-    if not hasattr(x1, "shape"):
-        return np.exp(-gamma  * np.linalg.norm(x1 - x2, ord=2)**2)
-    if np.asarray(x1).ndim == 0:
-        return np.exp(-gamma  * (x1 - x2)**2)
-    if len(x1.shape) >= 2 or len(x2.shape) >= 2:
-        K = np.zeros((x1.shape[0], x2.shape[0]))
-        for i, x in enumerate(x1):
-            for j, y in enumerate(x2):
+    if sp.isspmatrix(x) and sp.isspmatrix(y):
+        x = np.array(x.todense())
+        y = np.array(y.todense())
+    if not hasattr(x, "shape"):
+        return np.exp(-gamma  * np.linalg.norm(x - y, ord=2)**2)
+    if np.asarray(x).ndim == 0:
+        return np.exp(-gamma  * (x - y)**2)
+    if len(x.shape) >= 2 or len(y.shape) >= 2:
+        K = np.zeros((x.shape[0], y.shape[0]))
+        for i, x in enumerate(x):
+            for j, y in enumerate(y):
                 K[i, j] = np.exp(-gamma * np.linalg.norm(x - y, ord=2)**2)
         return K
-    return np.exp(-gamma  * np.linalg.norm(x1 - x2, ord=2)**2)
+    return np.exp(-gamma  * np.linalg.norm(x - y, ord=2)**2)
 
 rbf_kernel = exponential_kernel
 
@@ -99,21 +127,28 @@ rbf_kernel = exponential_kernel
 
 def random_kernel(n):
     """
-    :param n:
-        Number of examples.
-    :return:
-        Random positive-semidefinit kernel matrix.
-    """
-    T = np.random.rand(n, n)
-    return T.T.dot(T)
+    Generate a random kernel matrix of shape ``(n, n)``.
 
+    :param n: (``int``) Number of examples.
+
+    :return: (``numpy.ndarray``) Random positive semidefinite kernel matrix of shape ``(n, n)``.
+    """
+    G = np.random.rand(n, n)
+    return G.T.dot(G)
 
 def center_kernel(K):
     """
-    :param K:
-        Kernel matrix.
-    :return:
-        Centered kernel for a sample of points.
+    Center a kernel matrix.
+
+
+    .. math::
+        \mathbf{K}_{c} = (\mathbf{I}-\dfrac{\mathbf{11}^T}{n})\mathbf{K}(\mathbf{I}-\dfrac{\mathbf{11}^1}{n})
+        
+
+    :param K: (``numpy.ndarray``) Kernel matrix of shape ``(n, n)``.
+
+    :return: (``numpy.ndarray``) Centered kernel for a sample of points.
+
     """
     m = float(K.shape[0])
     o = np.ones((m, 1))
@@ -124,9 +159,13 @@ def center_kernel(K):
 
 def center_kernel_low_rank(G):
     """
-    :param G:
-        Low-rank approximation of the feature space
-    :return:
-        Centered low-rank approximation of the feature space
+    Center a the feature matrix such that :math:`\mathbf{G}_c \mathbf{G}_c^T` is centered.
+
+    .. math::
+        \mathbf{G}_c = (\mathbf{I} - \dfrac{\mathbf{11}^T}{n})\mathbf{G}
+
+    :param G: (``numpy.ndarray``) Low-rank approximation of the feature matrix of shape ``(n, k)``.
+
+    :return: (``numpy.ndarray``) Centered low-rank approximation of the feature space.
     """
     return G - G.mean(axis=0)
