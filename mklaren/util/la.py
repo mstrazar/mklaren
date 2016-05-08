a49083be5a9697e52fc6eb8f610f5e465df9678e
mstrazar <martin.strazar@gmail.com>
2016-05-08 02:21:36 +0200

    Docstrings to la module.

--- mklaren/util/la.py
+++ mklaren/util/la.py
@@ -1,5 +1,5 @@
 """
-    Linear algebra utility functions
+Linear algebra utility functions.
 """
 from numpy import multiply, array, where, isinf, sum as npsum, max as npmax
 from numpy import sqrt, random, eye, atleast_2d, zeros
@@ -7,58 +7,91 @@ from numpy.linalg import inv, norm
 from itertools import product
 
 
-def fro_prod(X1, X2):
+def fro_prod(A, B):
     """
-    :param X1: array/matrix.
-    :param X2: array/matrix.
-    :return: Frobenius product between X1 and X2.
+    The Frobenius product is an inner product between matrices :math:`\mathbf{A}` and :math:`\mathbf{B}` of same shape.
+
+    .. math::
+        <\mathbf{A}, \mathbf{B}>_F = \sum_{i, j} \mathbf{A}_{ij} \mathbf{B}_{ij}
+
+    :param A: (``numpy.ndarray``) a matrix.
+
+    :param B: (``numpy.ndarray``) a matrix.
+
+    :return: (``float``) Frobenius product value.
     """
-    return npsum(multiply(X1, X2))
+    return npsum(multiply(A, B))
 
 
-def fro_prod_low_rank(X1, X2):
+def fro_prod_low_rank(A, B):
     """
-    :param X1: array/matrix.
-    :param X2: array/matrix.
-    :return: Frobenius product between X1 and X2, where X1 and X2 are low
-             rank representations of square matrices X1 X1^T and X2 X2^T.
+    The Frobenius product of kernel matrices induced by linear kernels on :math:`\mathbf{A}` and :math:`\mathbf{B}`:
+
+    .. math::
+        <\mathbf{AA}^T, \mathbf{BB}^T>_F
+
+    Note :math:`A` and :math:`B` need not be of the same shape.
+
+    :param A: (``numpy.ndarray``) a matrix.
+
+    :param B: (``numpy.ndarray``) a matrix.
+
+    :return: (``float``) Frobenius product value.
     """
 
     fp = 0
-    for i in xrange(X1.shape[1]):
-        for j in xrange(X2.shape[1]):
-            gi = X1[:, i]
-            gj = X2[:, j]
+    for i in xrange(A.shape[1]):
+        for j in xrange(B.shape[1]):
+            gi = A[:, i]
+            gj = B[:, j]
             fp += gi.dot(gj)**2
     return fp
 
 
 def cosine_similarity(A, B):
     """
-    Cosine similarity between matrix A and B.
-    :param A: Matrix.
-    :param B: Matrix.
-    :return: Cosine similarity (kernel alignment).
+    Cosine similarity between matrices :math:`\mathbf{A}` and :math:`\mathbf{B}` defined in terms of the Frobenius product
+
+    .. math::
+        \dfrac{<\mathbf{A}, \mathbf{B}>_F} {<\mathbf{A}, \mathbf{A}>_F <\mathbf{B}, \mathbf{B}>_F}
+
+    :param A: (``numpy.ndarray``) a matrix.
+
+    :param B: (``numpy.ndarray``) a matrix.
+
+    :return: (``float``) Cosine similarity value.
     """
     return fro_prod(A, B) / sqrt(fro_prod(A, A) * fro_prod(B, B))
 
+
 def cosine_similarity_low_rank(a, b):
     """
-    Cosine similarity between matrices from outer products of vectors a, b.
-    :param a: Column vector.
-    :param b: Column vector.
-    :return: Cosine similarity (kernel alignment).
+    Cosine similarity between matrices from outer products of vectors :math:`\mathbf{a}` and :math:`\mathbf{b}`:
+
+    .. math::
+        \mathbf{A} = \mathbf{aa}^T\
+        \mathbf{B} = \mathbf{bb}^T
+
+    .. math::
+        \dfrac{<\mathbf{A}, \mathbf{B}>_F} {<\mathbf{A}, \mathbf{A}>_F <\mathbf{B}, \mathbf{B}>_F}
+
+    :return: (``float``) Cosine similarity value.
     """
     enum = a.T.dot(b)**2
     denom = a.T.dot(a) * b.T.dot(b)
     return  1.0 * enum / denom
 
+
 def cosine_similarity_low_rank_multi(G, y):
     """
-    Cosine similarity between matrices from outer products of matrix G and vector y.
-    :param G: Low-rank matrix.
-    :param y: Column vector.
-    :return: Cosine similarity (kernel alignment).
+    Cosine similarity between matrices from outer products of matrix :math:`\mathbf{G}` and vector :math:`\mathbf{y}`.
+
+    :param G: (``numpy.ndarray``) Low-rank matrix.
+    
+    :param y: (``numpy.ndarray``) Column vector.
+    
+    :return: (``float``) Cosine similarity (kernel alignment).
+    
     """
     enum = npsum(G.T.dot(y)**2)
     denom = y.T.dot(y) * sqrt(npsum([npsum(G[:, i] * G[:, j])**2
@@ -68,19 +101,24 @@ def cosine_similarity_low_rank_multi(G, y):
 
 def normalize(X):
     """
-    Normalize X to have values between 0 and 1.
-    :param X: Data matrix.
-    :return: Normalize matrix.
+    Normalize :math:`\mathbf{X}` to have values between 0 and 1.
+
+    :param X: (``numpy.ndarray``) Data matrix.
+
+    :return: (``numpy.ndarray``) Normalize matrix.
     """
     return X / npmax(X)
 
 
 def safe_divide(A, b):
     """
-    Perform a safe float division between a, b
-    :param A: Scalar / Matrix
-    :param b: Scalar / Matrix
-    :return: Matrix divided by b if b not zero else 0
+    Perform a safe float division between :math:`\mathbf{A}` and :math:`\mathbf{b}`.
+
+    :param A: (``numpy.ndarray``) Scalar / Matrix.
+
+    :param b: (``numpy.ndarray``) Scalar / Matrix.
+
+    :return: (``float``) Matrix divided by b if b not zero else 0.
     """
     if isinstance(b, type(array([]))):
         D = 1.0 * multiply(A, b > 0)
@@ -95,14 +133,14 @@ def safe_divide(A, b):
 def safe_func(x, f, val=0):
     """
     Evaluate a function defined on positive reals.
-    :param x:
-        Value.
-    :param f:
-        Function (callable).
-    :param val
-        Return value when x is out of domain.
-    :return:
-        Funciton value or out of domain.
+
+    :param x: (``float``) a value.
+
+    :param f: (``callable``) Function to evaluate.
+
+    :param val: (``float``) Return value when ``x`` is out of domain.
+
+    :return: (``float``) Function value or out of domain.
     """
 
     if isinstance(x, type(array([]))):
@@ -114,31 +152,36 @@ def safe_func(x, f, val=0):
     return f(x)
 
 
-def outer_product(u, v):
+def outer_product(a, b):
     """
-    Outer product between matrices/vectors u, v.
-    :param u: Vector / Matrix
-    :param v: Vector / Matrix
-    :return: Outer product of the appropriate dimension.
+    Outer product between vectors :math:`\mathbf{a}` and :math:`\mathbf{b}`:
+
+    :param a: (``numpy.ndarray``) column vector.
+
+    :param b: (``numpy.ndarray``) column vector.
+
+    :return: (``float``) Outer product of the appropriate dimension.
     """
-    if len(u.shape) == 1 and len(v.shape) == 1:
-        return u.reshape((len(u), 1)).dot(v.reshape((1, len(v))))
+    if len(a.shape) == 1 and len(b.shape) == 1:
+        return a.reshape((len(a), 1)).dot(b.reshape((1, len(b))))
     else:
-        return atleast_2d(u).dot(atleast_2d(v).T)
+        return atleast_2d(a).dot(atleast_2d(b).T)
 
 
 def woodbury_inverse(G, sigma2):
     """
-    Matrix inversion lemma Woodbury-Sherman-Morrison.
+    Matrix inversion using the Woodbury-Sherman-Morrison lemma.
 
-        inv((G * G.T)^2 + sigma**-1 * I)
+    .. math::
+        (\mathbf{GG}^T + \dfrac{1}{\sigma^2}  \mathbf{I})^{-1}
 
     Order of operations is important.
 
-    :param G: Rank-k approximation for symmetric matrix.
-    :param sigma2: Prior variance (sigma squared).
-    :return:
-        The solution to the above equation by inverting k x k matrix.
+    :param G: (``numpy.ndarray``) Low-rank matrix.
+
+    :param sigma2: (``float``) Noise / nugget term / sigma squared.
+
+    :return: (``numpy.ndarray``) The solution to the above equation by inverting a shape ``(k, k)`` matrix.
     """
     assert sigma2 > 0
     n, k    = G.shape
@@ -150,53 +193,60 @@ def woodbury_inverse_full(Ai, U, Ci, V):
     """
     Matrix inversion lemma Woodbury-Sherman-Morrison in its general form.
 
-        inv(A + UCV)
+    .. math::
+        (\mathbf{A} + \mathbf{UCV})^{-1}
 
-    :param Ai: Inverse of a square matrix n x n.
-    :param U: Matrix n x k
-    :param Ci: Inverse of square matrix k x k.
-    :param V: Matrix k x n
-    :return:
-        The solution to the above equation by inverting A and C.
+    :param Ai: (``numpy.ndarray``)  Inverse of a square matrix of shape ``(n, n)``.
+
+    :param U: (``numpy.ndarray``)  Matrix of shape ``(n, k)``.
+
+    :param Ci: (``numpy.ndarray``)  Inverse of square matrix of shape ``(k, k)``.
+
+    :param V: (``numpy.ndarray``)  Matrix of shape ``(k, n)``.
+
+    :return: (``numpy.ndarray``)  The solution to the above equation by inverting A and C. The solution is of shape ``(n, n)``.
     """
     return Ai - Ai.dot(U).dot(inv(Ci + V.dot(Ai).dot(U))).dot(V.dot(Ai))
 
 
-def covariance_full_rank(C, sigma2):
+def covariance_full_rank(K, sigma2):
     """
-    Complete covariance matrix to full rank by adding an indetity matrix.
+    Complete the kernel/covariance matrix to full rank by adding an indetity matrix.
 
-    :param C:
-        Symetric covariance matrix n x n.
-    :param sigma2:
-        Added covariance.
-    :return:
-        C + sigma2 * I.
+    .. math::
+        (\mathbf{K} + \sigma^2  \mathbf{I})^{-1}
+
+    :param K: (``numpy.ndarray``)  Kernel matrix of shape ``(n, n)``.
+
+    :param sigma2: (``float``) Added covariance.
+
+    :return: (``numpy.ndarray``) Modified kernel matrix.
     """
-    n = C.shape[0]
-    return C + sigma2 * array(eye(n, n))
+    n = K.shape[0]
+    return K + sigma2 * array(eye(n, n))
 
 
-def ensure_symmetric(C):
+def ensure_symmetric(K):
     """
-    Ensure a symmetric matrix.
-    Small deviations in symmetry can present obstacles to numpy.svd used
-    by samplers.
+    Ensure a symmetric matrix. Small deviations in symmetry can present obstacles to ``numpy.svd`` used by samplers.
 
-    :param C:
-    :return:
-        Correction to C up to symmetry.
+    .. math:
+        \mathbf{K} = \dfrac{\math{K} + \math{K}^T}{2}
+
+    :param K: (``numpy.ndarray``)  Kernel matrix of shape ``(n, n)``.
+
+    :return: (``numpy.ndarray``)  Correction to C up to symmetry.
     """
-    return (C + C.T) / 2.0
+    return (K + K.T) / 2.0
 
 
 def qr(A):
     """
     Fast QR decomposition.
-    :param A: m x n matrix
-    :return:
-        Q orthonormal vectors
-        R upper triangular
+
+    :param A: (``numpy.ndarray``)  A matrix of shape ``(m, n)``.
+
+    :return: (``tuple``) of (``numpy.ndarray``) orthonormal vectors (Q)  and (``numpy.ndarray``) upper triangular matrix (R).
     """
     m, n = A.shape
     Q = zeros((m, n))
