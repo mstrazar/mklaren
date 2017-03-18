851c1ebac00730503385db2bc20dffc3539cb969
mstrazar <martin.strazar@gmail.com>
2017-03-18 23:21:00 +0100

    Prediction with polynomial kernels (draft 2).

--- examples/polynomial_prediction.py
+++ examples/polynomial_prediction.py
@@ -6,6 +6,7 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 
 # TODO: enable prediction with sums of kernels in CSI for comparison
+# TODO: enable row normalization for kernels
 
 # Fixed hyper parameters
 delta = 10
@@ -13,7 +14,7 @@ repeats = 10
 range_n = [30, 100, 300]
 range_degree = range(2, 6)
 methods = ["Mklaren", "CSI"]
-rank = 5   # Rank is known because the data is simulated
+rank = 10   # Rank is known because the data is simulated
 p_tr = 0.75
 p_te = 1.0 - p_tr
 P = 1   # Number of true kernels to be taken in the sum
@@ -54,14 +55,15 @@ for method in methods:
 
     # Fit the mklaren method and predict
     if method == "Mklaren":
-        model_mklaren = Mklaren(rank=len(Ks_tr) * rank, delta=delta, lbd=lbd)
+        model_mklaren = Mklaren(rank=rank, delta=delta, lbd=lbd)
         model_mklaren.fit(Ks_tr, y_true[tr])
         y_pred = model_mklaren.predict([X_te] * len(Ks_tr))
         y_fit = model_mklaren.predict([X_tr] * len(Ks_tr))
         w_fit = model_mklaren.mu / model_mklaren.mu.sum()
     elif method == "CSI":
         model_csi = RidgeLowRank(rank=rank, method="csi", lbd=lbd,
-                                 method_init_args={"delta": delta})
+                                 method_init_args={"delta": delta},
+                                 sum_kernels=True)
         model_csi.fit(Ks_tr, y_true[tr])
         y_pred = model_csi.predict([X_te] * len(Ks_tr))
         y_fit = model_csi.predict([X_tr] * len(Ks_tr))
