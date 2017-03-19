fdfebaff9d6342c44dabd81947d2f2ba0a5b0f52
mstrazar <martin.strazar@gmail.com>
2017-03-19 12:27:36 +0100

    Systematic prediction with polynomial kernels.

---
+++ examples/polynomial_prediction.R
@@ -0,0 +1,40 @@
+require(ggplot2)
+data = read.csv("output/polynomial_prediction/2017-3-19/results_2.csv", header=TRUE)
+
+# Graphical plots
+qplot(data=data, x=as.factor(D), y=mse_pred, fill=method, geom="boxplot")
+ggsave("output/polynomial_prediction/mse_pred_degree.pdf")
+qplot(data=data, x=as.factor(D), y=mse_fit, fill=method, geom="boxplot")
+ggsave("output/polynomial_prediction/mse_fit_degree.pdf")
+
+qplot(data=data, x=as.factor(lbd), y=mse_fit, fill=method, geom="boxplot")
+ggsave("output/polynomial_prediction/mse_fit_lbd.pdf")
+qplot(data=data, x=as.factor(lbd), y=mse_pred, fill=method, geom="boxplot")
+ggsave("output/polynomial_prediction/mse_pred_lbd.pdf")
+
+# Wilcoxon rank.test (depending on rank)
+for (target in c("mse_fit", "mse_pred")){
+  rank.results = data.frame()
+  for (d in c(unique(data$D), "all")){
+    inxs.mkl = data$method=="Mklaren"
+    inxs.csi = data$method=="CSI"
+    if (d != "all"){
+      inxs.mkl = inxs.mkl & data$D == as.numeric(d)
+      inxs.csi = inxs.csi & data$D == as.numeric(d)
+    }
+    mse.mkl = data[inxs.mkl, target]
+    mse.csi = data[inxs.csi, target]
+
+    t = wilcox.test(mse.mkl, mse.csi, paired = TRUE, alternative = "less")
+    mark = ""
+    if(t$p.value < 0.05) mark = "*"
+    if(t$p.value < 0.01) mark = "**"
+    if(t$p.value < 0.001) mark = "***"
+
+    df = data.frame(degree=d, Wp=t$p.value, mark=mark)
+    rank.results = rbind(rank.results, df)
+  }
+  fname = sprintf("output/polynomial_prediction/wilcox.degree_%s.tab", target)
+  write.table(rank.results, fname, row.names=FALSE)
+  message(sprintf("Written %s", fname))
+}
--- examples/polynomial_prediction.py
+++ examples/polynomial_prediction.py
@@ -3,72 +3,107 @@ from mklaren.kernel.kinterface import Kinterface
 from mklaren.mkl.mklaren import Mklaren
 from mklaren.regression.ridge import RidgeLowRank
 from sklearn.metrics import mean_squared_error
+from itertools import product
 import numpy as np
-
-# TODO: enable prediction with sums of kernels in CSI for comparison
-# TODO: enable row normalization for kernels
+import csv
+import datetime
+import os
 
 # Fixed hyper parameters
-delta = 10
 repeats = 10
 range_n = [30, 100, 300]
-range_degree = range(2, 6)
-methods = ["Mklaren", "CSI"]
+range_degree = range(2, 7)
+range_repeat = range(repeats)
+range_lbd = [0, 1, 10]
+methods = ["Mklaren", "CSI", "ICD"]
+delta = 10  # Delta to max rank
 rank = 10   # Rank is known because the data is simulated
 p_tr = 0.75
 p_te = 1.0 - p_tr
 P = 1   # Number of true kernels to be taken in the sum
-lbd = 0
 
-n = 30
-maxd = 2
 
-# Training / test split
-tr = range(int(n * p_tr))
-te = range(tr[-1]+1, n)
+# Create output directory
+d = datetime.datetime.now()
+dname = os.path.join("output", "polynomial_prediction", "%d-%d-%d" % (d.year, d.month, d.day))
+if not os.path.exists(dname):
+    os.makedirs(dname)
+fname = os.path.join(dname, "results_%d.csv" % len(os.listdir(dname)))
+print("Writing to %s ..." % fname)
 
-X = np.random.rand(n, rank)
-X = (X - X.mean(axis=0)) / np.std(X, axis=0)
+header = ["repl", "method", "mse_fit", "mse_pred", "lbd", "n", "D"]
+writer = csv.DictWriter(open(fname, "w", buffering=0),
+                        fieldnames=header, quoting=csv.QUOTE_ALL)
+writer.writeheader()
 
-X_tr = X[tr]
-X_te = X[te]
+count = 0
+for repl, n, lbd, maxd in product(range_repeat, range_n, range_lbd, range_degree):
 
-Ks_tr = []
-Ks_all = []
-for d in range(1, maxd + 1):
-    K_tr = Kinterface(kernel=poly_kernel, kernel_args={"degree": d}, data=X_tr)
-    K_a = Kinterface(kernel=poly_kernel, kernel_args={"degree": d}, data=X)
-    Ks_tr.append(K_tr)
-    Ks_all.append(K_a)
+    # Training / test split
+    tr = range(int(n * p_tr))
+    te = range(tr[-1]+1, n)
 
-mu_true = np.zeros((len(Ks_tr),))
-mu_true[-P] = 1
+    X = np.random.rand(n, rank)
+    X = (X - X.mean(axis=0)) / np.std(X, axis=0)
 
-# True kernel matrix to generate the signal ;
-# weights are defined only by the training set
-alpha = np.random.rand(n, 1)
-alpha[te] = 0
-K_true = sum([mu_true[i] * Ks_all[i][:, :] for i in range(len(Ks_all))])
-y_true = K_true.dot(alpha)
+    X_tr = X[tr]
+    X_te = X[te]
 
-for method in methods:
+    Ks_tr = []
+    Ks_all = []
+    for d in range(1, maxd + 1):
+        K_tr = Kinterface(kernel=poly_kernel, kernel_args={"degree": d},
+                          data=X_tr, row_normalize=True)
+        K_a = Kinterface(kernel=poly_kernel, kernel_args={"degree": d},
+                         data=X, row_normalize=True)
+        Ks_tr.append(K_tr)
+        Ks_all.append(K_a)
 
-    # Fit the mklaren method and predict
-    if method == "Mklaren":
-        model_mklaren = Mklaren(rank=rank, delta=delta, lbd=lbd)
-        model_mklaren.fit(Ks_tr, y_true[tr])
-        y_pred = model_mklaren.predict([X_te] * len(Ks_tr))
-        y_fit = model_mklaren.predict([X_tr] * len(Ks_tr))
-        w_fit = model_mklaren.mu / model_mklaren.mu.sum()
-    elif method == "CSI":
-        model_csi = RidgeLowRank(rank=rank, method="csi", lbd=lbd,
-                                 method_init_args={"delta": delta},
-                                 sum_kernels=True)
-        model_csi.fit(Ks_tr, y_true[tr])
-        y_pred = model_csi.predict([X_te] * len(Ks_tr))
-        y_fit = model_csi.predict([X_tr] * len(Ks_tr))
+    mu_true = np.zeros((len(Ks_tr),))
+    mu_true[-P] = 1
 
-    # Score the predictions
-    mse_pred = mean_squared_error(y_true[te], y_pred)
-    mse_fit = mean_squared_error(y_true[tr], y_fit)
-    print(method, mse_pred, mse_fit)
+    # True kernel matrix to generate the signal ;
+    # weights are defined only by the training set
+    alpha = np.random.rand(n, 1)
+    alpha[te] = 0
+    K_true = sum([mu_true[i] * Ks_all[i][:, :] for i in range(len(Ks_all))])
+    y_true = K_true.dot(alpha)
+
+    rows = []
+    for method in methods:
+        # Fit the mklaren method and predict
+        try:
+            if method == "Mklaren":
+                model_mklaren = Mklaren(rank=rank, delta=delta, lbd=lbd)
+                model_mklaren.fit(Ks_tr, y_true[tr])
+                y_pred = model_mklaren.predict([X_te] * len(Ks_tr))
+                y_fit = model_mklaren.predict([X_tr] * len(Ks_tr))
+                w_fit = model_mklaren.mu / model_mklaren.mu.sum()
+            elif method == "CSI":
+                model_csi = RidgeLowRank(rank=rank, method="csi", lbd=lbd,
+                                         method_init_args={"delta": delta},
+                                         sum_kernels=True)
+                model_csi.fit(Ks_tr, y_true[tr])
+                y_pred = model_csi.predict([X_te] * len(Ks_tr))
+                y_fit = model_csi.predict([X_tr] * len(Ks_tr))
+            elif method == "ICD":
+                model_icd = RidgeLowRank(rank=rank, method="icd", lbd=lbd,
+                                         sum_kernels=True)
+                model_icd.fit(Ks_tr, y_true[tr])
+                y_pred = model_icd.predict([X_te] * len(Ks_tr))
+                y_fit = model_icd.predict([X_tr] * len(Ks_tr))
+        except:
+            print("%s error" % method)
+            continue
+
+        # Score the predictions
+        mse_pred = mean_squared_error(y_true[te], y_pred)
+        mse_fit = mean_squared_error(y_true[tr], y_fit)
+        row = {"repl": repl, "method": method, "mse_fit": mse_fit, "mse_pred": mse_pred,
+               "lbd": lbd, "n": n, "D": maxd}
+        rows.append(row)
+
+    if len(rows) == len(methods):
+        count += len(rows)
+        writer.writerows(rows)
+        print("Written %d rows" % count)
