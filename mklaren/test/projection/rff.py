b9b3d456e9711b1dd2bc03c15a4d6ba40cfa7227
mstrazar <martin.strazar@gmail.com>
2017-04-02 12:41:57 +0200

    Added L2 regularization, via lemma Zou & Hastie (2005).

--- mklaren/test/projection/rff.py
+++ mklaren/test/projection/rff.py
@@ -11,6 +11,7 @@ class TestRFF(unittest.TestCase):
         self.rank_range = [10, 30, 50, 90]
         self.X = np.random.rand(self.n, self.p)
         self.gamma_range = np.logspace(-2, 2, 5)
+        self.lbd_range = np.logspace(-2, 2, 5)
 
         # Generate a random signal by employing one of the kernels
         K = exponential_kernel(self.X, self.X, gamma=self.gamma_range[1])
@@ -22,7 +23,7 @@ class TestRFF(unittest.TestCase):
         """
         Fitted features must match transformed ones.
         """
-        model = RFF(delta=10, rank=30, gamma_range=self.gamma_range)
+        model = RFF(delta=10, rank=30, gamma_range=self.gamma_range, lbd=0)
         model.fit(self.X, self.y)
         G = model.G
         Gt = model.transform(self.X)
@@ -38,8 +39,31 @@ class TestRFF(unittest.TestCase):
 
         for ri, rank in enumerate(self.rank_range):
             model = RFF(delta=10, rank=rank,
-                        gamma_range=self.gamma_range, random_state=42)
+                        gamma_range=self.gamma_range, random_state=42,
+                        lbd=0)
             model.fit(self.X, self.y)
             yp = model.predict(self.X)
             errors[ri] = np.sum((self.y.ravel() - yp.ravel()) ** 2)
         self.assertTrue(np.all(errors[:-1] > errors[1:]))
+
+    def testRegularization(self):
+        """
+        Reconstruction must improve with increasing rank and
+        random initial conditions
+        """
+        errors = np.zeros((len(self.lbd_range),))
+        beta_norms = np.zeros((len(self.lbd_range),))
+
+        for li, lbd in enumerate(self.lbd_range):
+            model = RFF(delta=10, rank=30,
+                        gamma_range=self.gamma_range, random_state=42,
+                        lbd=lbd)
+            model.fit(self.X, self.y)
+            yp = model.predict(self.X)
+            errors[li] = np.sum((self.y.ravel() - yp.ravel()) ** 2)
+            beta_norms[li] = np.linalg.norm(model.beta)
+
+        # Error grows with lambda
+        # Vector norm drops
+        self.assertTrue(np.all(errors[:-1] < errors[1:]))
+        self.assertTrue(np.all(beta_norms[:-1] > beta_norms[1:]))
