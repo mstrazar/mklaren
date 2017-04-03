fb5c86f96899f65aae65cda8941009f0b86de690
mstrazar <martin.strazar@gmail.com>
2017-04-03 10:21:57 +0200

    Added Friedman test.

--- examples/polynomial_prediction.R
+++ examples/polynomial_prediction.R
@@ -44,57 +44,57 @@ qplot(data=data, x=as.factor(rank), y=mse_pred, fill=method, geom="boxplot")
 ggsave("output/polynomial_prediction/mse_pred_rank.pdf")
 
 # Wilcoxon rank.test (depending on rank)
-for (target in c("mse_pred")){
-  for (meth in c("CSI", "Nystrom", "ICD", "RFF")){
-    rank.results = data.frame()
-    for (d in c(unique(data$D), "all")){
-      inxs.mkl = data$method=="Mklaren"
-      inxs.csi = data$method==meth
-      if (d != "all"){
-        inxs.mkl = inxs.mkl & data$D == as.numeric(d)
-        inxs.csi = inxs.csi & data$D == as.numeric(d)
-      }
-      mse.mkl = data[inxs.mkl, target]
-      mse.csi = data[inxs.csi, target]
-  
-      t = wilcox.test(mse.mkl, mse.csi, paired = TRUE, alternative = "less")
-      mark = ""
-      if(t$p.value < 0.05) mark = "*"
-      if(t$p.value < 0.01) mark = "**"
-      if(t$p.value < 0.001) mark = "***"
-  
-      df = data.frame(degree=d, Wp=t$p.value, mark=mark)
-      rank.results = rbind(rank.results, df)
+target = "mse_pred"
+for (meth in c("CSI", "Nystrom", "ICD", "RFF")){
+  rank.results = data.frame()
+  for (d in c(unique(data$D), "all")){
+    inxs.mkl = data$method=="Mklaren"
+    inxs.csi = data$method==meth
+    if (d != "all"){
+      inxs.mkl = inxs.mkl & data$D == as.numeric(d)
+      inxs.csi = inxs.csi & data$D == as.numeric(d)
     }
-  fname = sprintf("output/polynomial_prediction/wilcox.degree_%s_%s.tab", target, meth)
-  write.table(rank.results, fname, row.names=FALSE)
-  message(sprintf("Written %s", fname))
-  }
-}
+    mse.mkl = data[inxs.mkl, target]
+    mse.csi = data[inxs.csi, target]
 
-# Friedman rank test ; reshape the matrix into experiments/rows form
-# Warning: subsequent rows are assumed to belong to the same experiment
-lvls = levels(data$method)
-p = length(lvls)
-for (d in c("all", unique(data$D))){
-  if(d == "all"){
-    dd = data 
-  } else {
-    dd = data[data$D == d, ]
+    t = wilcox.test(mse.mkl, mse.csi, paired = TRUE, alternative = "less")
+    mark = ""
+    if(t$p.value < 0.05) mark = "*"
+    if(t$p.value < 0.01) mark = "**"
+    if(t$p.value < 0.001) mark = "***"
+
+    df = data.frame(degree=d, Wp=t$p.value, mark=mark)
+    rank.results = rbind(rank.results, df)
   }
-  D = matrix(0, nrow=nrow(dd)/p, ncol=p)
-  colnames(D) = lvls
-  for (i in 0:nrow(D)-1){
-    df = dd[(i*p):((i+1)*p - 1), ]
-    D[i, df$method] = df$mse_pred
-  }  
-  fname = sprintf("output/polynomial_prediction/cd.degree_%s.pdf", d)
-  # pdf(fname)
-  plotCD(-D, alpha=0.05, cex=1.25)
-  title(sprintf("Degree: %s", d))
-  # dev.off()
-  message(sprintf("Written %s", fname))
+fname = sprintf("output/polynomial_prediction/wilcox.degree_%s_%s.tab", target, meth)
+write.table(rank.results, fname, row.names=FALSE)
+message(sprintf("Written %s", fname))
 }
+  
+  
+  # Friedman rank test ; reshape the matrix into experiments/rows form
+  # Warning: subsequent rows are assumed to belong to the same experiment
+  lvls = levels(data$method)
+  p = length(lvls)
+  for (d in c("all", unique(data$D))){
+    if(d == "all"){
+      dd = data 
+    } else {
+      dd = data[data$D == d, ]
+    }
+    D = matrix(0, nrow=nrow(dd)/p, ncol=p)
+    colnames(D) = lvls
+    for (i in 0:nrow(D)-1){
+      df = dd[(i*p):((i+1)*p - 1), ]
+      D[i, df$method] = df$mse_pred
+    }  
+    fname = sprintf("output/polynomial_prediction/cd.degree_%s.pdf", d)
+    pdf(fname)
+    plotCD(-D, alpha=0.05, cex=1.25)
+    title(sprintf("Degree: %s", d))
+    dev.off()
+    message(sprintf("Written %s", fname))
+  }
 
 
 
