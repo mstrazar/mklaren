ce8abcfcb4af42e6ffc0197e30f847717392af5d
mstrazar <martin.strazar@gmail.com>
2017-04-06 15:52:41 +0200

    Draft timing experiment;

--- examples/timing.py
+++ examples/timing.py
@@ -14,18 +14,20 @@ repeats = 10
 range_n = np.linspace(1e3, 1e6, 4)
 range_degree = [6]
 range_repeat = range(repeats)
-range_lbd = [0.01, 0.1, 1, 10]
 range_rank = [30]
 sigma2 = 0.1    # noise variance
 lbd = 0.1
 
-methods = ["Mklaren"]
+methods = ["Mklaren", "ICD", "Nystrom"]
 delta = 10  # Delta to max rank
 P = 1   # Number of true kernels to be taken in the sum
 p_tr = 0.6
 p_va = 0.2
 p_te = 0.2
 
+
+# TODO: to ensure a practical comparison, run the methods until some practical
+# criterion is achieved w.r.t. prediction performance
 
 
 # Create output directory
@@ -54,31 +56,36 @@ for repl, n, maxd, rank in product(range_repeat, range_n, range_degree, range_ra
     y_true = y_true - y_true.mean()
 
     Ks_all = []
-    for d in range(0, maxd + 1):
+    for d in range(maxd, maxd + 1):
         K_a = Kinterface(kernel=poly_kernel, kernel_args={"degree": d},
                          data=X, row_normalize=False)
         Ks_all.append(K_a)
     rows = []
     for method in methods:
         valid = True
-        try:
+        if True: # try:
             t1 = time()
             if method == "Mklaren":
-                model = Mklaren(rank=rank, delta=delta, lbd=lbd)
+                effective_rank = rank * len(Ks_all)
+                model = Mklaren(rank=effective_rank, delta=delta, lbd=lbd)
                 model.fit(Ks_all, y_true)
             elif method == "CSI":
                 model = RidgeLowRank(rank=rank, method="csi", lbd=lbd,
                                          method_init_args={"delta": delta},
-                                         sum_kernels=True)
+                                         sum_kernels=False)
                 model.fit(Ks_all, y_true)
             elif method == "ICD":
                 model = RidgeLowRank(rank=rank, method="icd", lbd=lbd,
-                                         sum_kernels=True)
+                                         sum_kernels=False)
+                model.fit(Ks_all, y_true)
+            elif method == "Nystrom":
+                model = RidgeLowRank(rank=rank, method="nystrom", lbd=lbd,
+                                         sum_kernels=False)
                 model.fit(Ks_all, y_true)
             t2 = time() - t1
-        except:
-            print("%s error" % method)
-            continue
+        # except:
+        #    print("%s error" % method)
+        #    continue
 
         # Score the predictions
         if valid:
