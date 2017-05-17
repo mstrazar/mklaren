d5a9640f3a32fb1d6a23527ff96ea89d97a99c58
mstrazar <martin.strazar@gmail.com>
2017-05-17 23:03:41 +0200

    Provide a fair comparison across multiple ranks and N.

--- examples/snr.py
+++ examples/snr.py
@@ -5,6 +5,14 @@ this leads to lower errors at high noise levels.
 Comparison of the three pivot selection methods with varying noise rates
 on a simple Gaussian Process signal.
 
+There is no training and test set, just comparison with recovering true
+signal.
+
+How to select lambda?
+    - Display results for all lambda.
+
+Add more kernels?
+
 """
 import matplotlib
 matplotlib.use("Agg")
@@ -15,7 +23,7 @@ import os
 import itertools as it
 
 import numpy as np
-from scipy.stats import multivariate_normal as mvn
+from scipy.stats import multivariate_normal as mvn, pearsonr
 from mklaren.kernel.kernel import exponential_kernel
 from mklaren.kernel.kinterface import Kinterface
 from mklaren.mkl.mklaren import Mklaren
@@ -33,27 +41,31 @@ if not os.path.exists(dname):
 fname = os.path.join(dname, "results_%d.csv" % len(os.listdir(dname)))
 print("Writing to %s ..." % fname)
 
-header = ["repl", "method", "n", "lbd", "snr", "rank", "noise", "mse_sig"]
+header = ["repl", "method", "n", "lbd", "snr", "rank", "noise", "mse_sig", "pr_rho", "pr_pval"]
 writer = csv.DictWriter(open(fname, "w", buffering=0),
                         fieldnames=header, quoting=csv.QUOTE_ALL)
 writer.writeheader()
 
 # Parameters
-n_range = [30, 100, 300, 1000]
-noise_range = np.logspace(-2, 2, 10)
-repeats = range(10)
-gamma = 1
-rank = 4
+gamma = 1                               # Arbitrary kernel hyperparameter
+delta = 10                              # Arbitrary look-ahead parameter
+
+# Objective experimentation;
+n_range = [30, 100, 300, 1000]          # Vaste enough range of dataset sizes (which are full-rank)
+noise_range = np.logspace(-3, 3, 7)     # Range of noise levels
+repeats = range(10)                     # Number of repeats
+rank_percents = [0.05, 0.1, 0.3]        # Rank percentages given n
 methods = ["Mklaren", "CSI", "ICD"]
+lbd_range = [0, 0.1, 0.3, 1, 3, 10]     # Vast enough range, such that methods should be able to capture optimum somewhere
+
+
 count = 0
-lbd_range = [0, 1, 3]
-
-
-for repl, n, noise, lbd in it.product(repeats, n_range, noise_range, lbd_range):
+for repl, n, noise, lbd, rp in it.product(repeats, n_range, noise_range, lbd_range, rank_percents):
+    rank = max(5, int(rp * n))
 
     # Generate data
     X = np.linspace(-10, 10, n).reshape((n, 1))
-    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 1})
+    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": gamma})
 
     f = mvn.rvs(mean=np.zeros((n, )), cov=K[:, :])
     y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
@@ -62,30 +74,37 @@ for repl, n, noise, lbd in it.product(repeats, n_range, noise_range, lbd_range):
     yp = None
     rows = []
     for method in methods:
-        if method == "Mklaren":
-            mkl = Mklaren(rank=rank,
-                          delta=10, lbd=lbd)
-            mkl.fit([K], y)
-            yp = mkl.predict([X])
+        # The difference is only in including lambda at the time of pivot selection
+        try:
+            if method == "Mklaren":
+                mkl = Mklaren(rank=rank,
+                              delta=delta, lbd=lbd)
+                mkl.fit([K], y)
+                yp = mkl.predict([X])
 
-        elif method == "ICD":
-            icd = RidgeLowRank(rank=rank, lbd=lbd,
-                               method="icd")
-            icd.fit([K], y)
-            yp = icd.predict([X])
+            elif method == "ICD":
+                icd = RidgeLowRank(rank=rank, lbd=lbd,
+                                   method="icd")
+                icd.fit([K], y)
+                yp = icd.predict([X])
 
-        elif method == "CSI":
-            icd = RidgeLowRank(rank=rank, lbd=lbd,
-                               method="csi", method_init_args={"delta": 10})
-            icd.fit([K], y)
-            yp = icd.predict([X])
+            elif method == "CSI":
+                icd = RidgeLowRank(rank=rank, lbd=lbd,
+                                   method="csi", method_init_args={"delta": delta})
+                icd.fit([K], y)
+                yp = icd.predict([X])
+        except Exception as e:
+            print("%s exception: %s" % (method,  e.message))
+            continue
 
-
+        # Metrics
         mse_sig = mse(yp, f)
         snr = np.var(f) / noise
+        pr_rho, pr_pval = pearsonr(yp, f)
 
         row = {"repl": repl, "method": method, "n": n, "snr": snr, "lbd": lbd,
-               "rank": rank, "noise": np.round(np.log10(noise), 2), "mse_sig": mse_sig}
+               "rank": rank, "noise": np.round(np.log10(noise), 2), "mse_sig": mse_sig,
+               "pr_rho": pr_rho, "pr_pval": pr_pval}
         rows.append(row)
 
     if len(rows) == len(methods):
@@ -94,16 +113,46 @@ for repl, n, noise, lbd in it.product(repeats, n_range, noise_range, lbd_range):
         print("%s Written %d rows (n=%d)" % (str(datetime.datetime.now()), count, n))
 
 
+if False:
+    # Generate data
+    n = 300
+    lbd = 1
+    noise = 1
+    X = np.linspace(-10, 10, n).reshape((n, 1))
+    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 1})
+
+    f = mvn.rvs(mean=np.zeros((n,)), cov=K[:, :])
+    y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
+
+    # Fit MKL
+    mkl = Mklaren(rank=rank,
+                   delta=delta, lbd=lbd)
+    mkl.fit([K], y)
+    yp = mkl.predict([X])
+
+    # Fit ICD
+    icd = RidgeLowRank(rank=rank, lbd=lbd, method="icd")
+    icd.fit([K], y)
+    yp_icd = icd.predict([X])
+
+    # Fit CSI
+    csi = RidgeLowRank(rank=rank, lbd=lbd, method="csi", method_init_args={"delta": delta})
+    # csi.fit([K], y)
+    # yp_csi = csi.predict([X])
 
 
+    # Plot archive snippet
+    # Plot data
+    plt.figure()
+    plt.plot(X.ravel(), f, "--", color="gray")
+    plt.plot(X.ravel(), y, ".", color="black")
+    plt.plot(X.ravel(), yp, "-", color="blue", linewidth=2)
+    plt.plot(X.ravel(), yp_icd, "-", color="green", linewidth=2)
+    # plt.plot(X.ravel(), yp_csi, "-", color="red", linewidth=2)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
 
-# Plot archive snippet
-# Plot data
-# plt.figure()
-# plt.plot(X.ravel(), f, "--", color="gray")
-# plt.plot(X.ravel(), y, ".", color="black")
-# plt.plot(X.ravel(), y_mkl, "-", color="green", linewidth=2)
-# plt.plot(X.ravel(), y_icd, "-", color="blue", linewidth=2)
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.show()
+    print("MKL", pearsonr(yp, f))
+    # print("CSI", pearsonr(yp_csi, f))
+    print("ICD", pearsonr(yp_icd, f))
