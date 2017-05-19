3364818facbcfcb8b04d16154fff2b00948e52d3
mstrazar <martin.strazar@gmail.com>
2017-05-19 10:44:03 +0200

    Correct mapping noise to lambda.

--- examples/snr.R
+++ examples/snr.R
@@ -3,6 +3,7 @@ setwd("~/Dev/mklaren/examples")
 
 # The methods are different whether we are allowed to use regularization or not
 data = read.csv("output/snr/2017-5-17/results_5.csv", header=TRUE)  # all lbd & n; wider range of noise and lambda; iterate over rank.
+data = read.csv("output/snr/2017-5-18/results_1.csv", header=TRUE)  # all lbd & n; wider range of noise and lambda; iterate over rank.
 
 # Merge results in a table for a given n (depend on noise) - store numeric format
 methods = sort(as.character(unique(data$method)))
@@ -19,14 +20,21 @@ for (n in unique(data$n)){
     
     # Create a mapping for (noise level, n, rank, method) -> best lambda
     m2lbd = m[m$best, c("method", "noise",  "lbd")]
-    row.names(m2lbd) = sprintf("%s.%d", mm$method, mm$noise)
+    row.names(m2lbd) = sprintf("%s.%d", m2lbd$method, m2lbd$noise)
     dff = df[df$lbd == m2lbd[sprintf("%s.%d", df$method, df$noise), "lbd"], ]               
     
-    fname = sprintf("output/snr/images/cv_lambda_n-%d_rank-%d.pdf", n, rank)
     rp = as.numeric(round(100*(rank / n)))
+    fname = sprintf("output/snr/images/cv_lambda_n-%d_rank-%d.pdf", n, rank)
     qplot(data=dff, x=as.factor(noise), y=mse_sig, geom="boxplot", fill=method,
           main=sprintf("N=%d, rank=%d %%, no. repl=%d", n, rp, max(dff$repl)+1),
           xlab="Noise", ylab="MSE")
+          # xlab="Noise", ylab="Relative MSE (%)")
+    ggsave(fname)
+    
+    fname = sprintf("output/snr/images/cv_rel_lambda_n-%d_rank-%d.pdf", n, rank)
+    qplot(data=dff, x=as.factor(noise), y=mse_rel, geom="boxplot", fill=method,
+          main=sprintf("N=%d, rank=%d %%, no. repl=%d", n, rp, max(dff$repl)+1),
+          xlab="Noise", ylab="Relative MSE (%)")
     ggsave(fname)
   }
 }
