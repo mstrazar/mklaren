11fd0820cfd8ee6d062c3c6f721612c6127e933e
mstrazar <martin.strazar@gmail.com>
2017-05-24 11:55:11 +0200

    Sample data from a Gaussian process and compare sum vs. list of kernels.

--- examples/snr.py
+++ examples/snr.py
@@ -24,7 +24,7 @@ import itertools as it
 
 import numpy as np
 from scipy.stats import multivariate_normal as mvn, pearsonr
-from mklaren.kernel.kernel import exponential_kernel
+from mklaren.kernel.kernel import exponential_kernel, kernel_sum
 from mklaren.kernel.kinterface import Kinterface
 from mklaren.mkl.mklaren import Mklaren
 from mklaren.regression.ridge import RidgeLowRank
@@ -114,49 +114,76 @@ for repl, gamma, n, noise, lbd, rp in it.product(repeats, gamma_range, n_range,
         print("%s Written %d rows (n=%d)" % (str(datetime.datetime.now()), count, n))
 
 
-if False:
-    # Generate data
-    n = 300
-    lbd = 1
-    noise = 1
-    X = np.linspace(-10, 10, n).reshape((n, 1))
-    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 1})
+def test(n=100, noise=0.1, rank=10, lbd=0.1, seed=None):
+    """
+    Sample data from a Gaussian process and compare fits with the sum of kernels
+    versus list of kernels.
+    :param n: Number of data points.
+    :param noise:  Noise variance.
+    :param rank:  Approximation rank.
+    :param lbd: Regularization parameter.
+    :param seed: Random seed.
+    :return:
+    """
 
-    f = mvn.rvs(mean=np.zeros((n,)), cov=K[:, :])
+    if seed is not None:
+        np.random.seed(seed)
+
+    # small gamma, large lengthscale -> small gamma, small frequency
+    gamma_range = np.logspace(-1, 3, 5)
+
+    # Generate data
+    X = np.linspace(-10, 10, n).reshape((n, 1))
+
+    # Kernel sum
+    Ksum = Kinterface(data=X, kernel=kernel_sum,
+                   kernel_args={
+                       "kernels": [exponential_kernel] * len(gamma_range),
+                       "kernels_args": [{"gamma": g} for g in gamma_range]})
+
+    # Sum of kernels
+    Klist = [Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": g})
+            for g in gamma_range]
+
+    f = mvn.rvs(mean=np.zeros((n,)), cov=Ksum[:, :])
     y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
 
-    # Fit MKL
+    # Fit MKL for kernel sum and
     mkl = Mklaren(rank=rank,
-                   delta=delta, lbd=lbd)
-    mkl.fit([K], y)
-    yp = mkl.predict([X])
+                  delta=10, lbd=lbd)
+    mkl.fit(Klist, y)
+    yp_Klist = mkl.predict([X] * len(Klist))
+    Axs = [X[mkl.data.get(gi, {}).get("act", [])].ravel() for gi in range(len(gamma_range))]
 
-    # Fit ICD
-    icd = RidgeLowRank(rank=rank, lbd=lbd, method="icd")
-    icd.fit([K], y)
-    yp_icd = icd.predict([X])
+    mkl.fit([Ksum], y)
+    yp_Ksum = mkl.predict([X])
 
-    # Fit CSI
-    csi = RidgeLowRank(rank=rank, lbd=lbd, method="csi", method_init_args={"delta": delta})
-    # csi.fit([K], y)
-    # yp_csi = csi.predict([X])
+    # Frequency scales
+    ymin = int(np.absolute(np.min(y)))
+    Gxs = [np.linspace(-10, 10, 5 + 10 * g) for g in gamma_range]
+    Gys = range(-ymin-len(Gxs), -ymin)
 
+    # Correlation
+    rho_Klist, _ = pearsonr(yp_Klist, f)
+    rho_Ksum, _ = pearsonr(yp_Ksum, f)
 
-    # Plot archive snippet
-    # Plot data
+    # Plot a summary figure
     plt.figure()
-    plt.plot(X.ravel(), f, "--", color="gray")
-    plt.plot(X.ravel(), y, ".", color="black")
-    plt.plot(X.ravel(), yp, "-", color="blue", linewidth=2)
-    plt.plot(X.ravel(), yp_icd, "-", color="green", linewidth=2)
-    # plt.plot(X.ravel(), yp_csi, "-", color="red", linewidth=2)
-    plt.xlabel("x")
-    plt.ylabel("y")
+
+    # Plot signal
+    x = X.ravel()
+    plt.plot(x, y, "k.")
+    plt.plot(x, f, "r--")
+    plt.plot(x, yp_Klist, "g-", label="Klist ($\\rho$=%.2f)" % rho_Klist)
+    plt.plot(x, yp_Ksum, "b-", label="Ksum ($\\rho$=%.2f)" % rho_Ksum)
+
+    # Plot freqency scales
+    for gi, (gx, gy) in enumerate(zip(Gxs, Gys)):
+        plt.plot(gx, [gy] * len(gx), "|", color="gray")
+        if len(Axs[gi]):
+            print("Number of pivots at gamma  %d: %d" % (gi, len(Axs[gi])))
+            plt.plot(Axs[gi], [gy]*len(Axs[gi]), "x", color="green", markersize=6)
+    plt.title("n=%d, noise=%.3f, rank=%d, lambda=%0.3f" % (n, noise, rank, lbd))
+    plt.legend()
+    plt.xlim((-11, 11))
     plt.show()
-
-    print("MKL", pearsonr(yp, f))
-    # print("CSI", pearsonr(yp_csi, f))
-    print("ICD", pearsonr(yp_icd, f))
-
-    print("MKL", mse(yp, f))
-    print("ICD", mse(yp_icd, f))
