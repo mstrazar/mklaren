464fb233f81155f8e4ac803165b7b53400d66f2c
mstrazar <martin.strazar@gmail.com>
2017-05-25 15:04:55 +0200

    Script-like processing.

--- examples/snr.py
+++ examples/snr.py
@@ -33,19 +33,6 @@ import matplotlib.pyplot as plt
 
 
 
-# Create output directory
-d = datetime.datetime.now()
-dname = os.path.join("output", "snr", "%d-%d-%d" % (d.year, d.month, d.day))
-if not os.path.exists(dname):
-    os.makedirs(dname)
-fname = os.path.join(dname, "results_%d.csv" % len(os.listdir(dname)))
-print("Writing to %s ..." % fname)
-
-header = ["repl", "method", "n", "gamma", "lbd", "snr", "rank", "noise", "mse_sig", "mse_rel", "pr_rho", "pr_pval"]
-writer = csv.DictWriter(open(fname, "w", buffering=0),
-                        fieldnames=header, quoting=csv.QUOTE_ALL)
-writer.writeheader()
-
 # Parameters
 gamma_range = 2**(np.linspace(-2, 2, 5))  # Arbitrary kernel hyperparameters
 delta = 10                                # Arbitrary look-ahead parameter
@@ -58,60 +45,76 @@ rank_percents = [0.05, 0.1, 0.15]         # Rank percentages given n
 methods = ["Mklaren", "CSI", "ICD"]
 lbd_range = [0, 0.1, 0.3, 1, 3, 10, 30, 100]  # Vast enough range, such that methods should be able to capture optimum somewhere
 
-count = 0
-for repl, gamma, n, noise, lbd, rp in it.product(repeats, gamma_range, n_range,
-                                                 noise_range, lbd_range, rank_percents):
-    rank = max(5, int(rp * n))
+def process():
 
-    # Generate data
-    X = np.linspace(-10, 10, n).reshape((n, 1))
-    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": gamma})
+    # Create output directory
+    d = datetime.datetime.now()
+    dname = os.path.join("output", "snr", "%d-%d-%d" % (d.year, d.month, d.day))
+    if not os.path.exists(dname):
+        os.makedirs(dname)
+    fname = os.path.join(dname, "results_%d.csv" % len(os.listdir(dname)))
+    print("Writing to %s ..." % fname)
 
-    f = mvn.rvs(mean=np.zeros((n, )), cov=K[:, :])
-    y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
+    header = ["repl", "method", "n", "gamma", "lbd", "snr", "rank", "noise", "mse_sig", "mse_rel", "pr_rho", "pr_pval"]
+    writer = csv.DictWriter(open(fname, "w", buffering=0),
+                            fieldnames=header, quoting=csv.QUOTE_ALL)
+    writer.writeheader()
 
-    # Mklaren
-    yp = None
-    rows = []
-    for method in methods:
-        # The difference is only in including lambda at the time of pivot selection
-        try:
-            if method == "Mklaren":
-                mkl = Mklaren(rank=rank,
-                              delta=delta, lbd=lbd)
-                mkl.fit([K], y)
-                yp = mkl.predict([X])
 
-            elif method == "ICD":
-                icd = RidgeLowRank(rank=rank, lbd=lbd,
-                                   method="icd")
-                icd.fit([K], y)
-                yp = icd.predict([X])
+    count = 0
+    for repl, gamma, n, noise, lbd, rp in it.product(repeats, gamma_range, n_range,
+                                                     noise_range, lbd_range, rank_percents):
+        rank = max(5, int(rp * n))
 
-            elif method == "CSI":
-                icd = RidgeLowRank(rank=rank, lbd=lbd,
-                                   method="csi", method_init_args={"delta": delta})
-                icd.fit([K], y)
-                yp = icd.predict([X])
-        except Exception as e:
-            print("%s exception: %s" % (method,  e.message))
-            continue
+        # Generate data
+        X = np.linspace(-10, 10, n).reshape((n, 1))
+        K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": gamma})
 
-        # Metrics
-        mse_sig = mse(yp, f)
-        mse_rel = mse(yp, f) / np.var(y)
-        snr = np.var(f) / noise
-        pr_rho, pr_pval = pearsonr(yp, f)
+        f = mvn.rvs(mean=np.zeros((n, )), cov=K[:, :])
+        y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
 
-        row = {"repl": repl, "method": method, "n": n, "snr": snr, "lbd": lbd,
-               "rank": rank, "noise": np.round(np.log10(noise), 2), "mse_sig": mse_sig,
-               "mse_rel": mse_rel, "pr_rho": pr_rho, "pr_pval": pr_pval, "gamma": gamma}
-        rows.append(row)
+        # Mklaren
+        yp = None
+        rows = []
+        for method in methods:
+            # The difference is only in including lambda at the time of pivot selection
+            try:
+                if method == "Mklaren":
+                    mkl = Mklaren(rank=rank,
+                                  delta=delta, lbd=lbd)
+                    mkl.fit([K], y)
+                    yp = mkl.predict([X])
 
-    if len(rows) == len(methods):
-        writer.writerows(rows)
-        count += len(rows)
-        print("%s Written %d rows (n=%d)" % (str(datetime.datetime.now()), count, n))
+                elif method == "ICD":
+                    icd = RidgeLowRank(rank=rank, lbd=lbd,
+                                       method="icd")
+                    icd.fit([K], y)
+                    yp = icd.predict([X])
+
+                elif method == "CSI":
+                    icd = RidgeLowRank(rank=rank, lbd=lbd,
+                                       method="csi", method_init_args={"delta": delta})
+                    icd.fit([K], y)
+                    yp = icd.predict([X])
+            except Exception as e:
+                print("%s exception: %s" % (method,  e.message))
+                continue
+
+            # Metrics
+            mse_sig = mse(yp, f)
+            mse_rel = mse(yp, f) / np.var(y)
+            snr = np.var(f) / noise
+            pr_rho, pr_pval = pearsonr(yp, f)
+
+            row = {"repl": repl, "method": method, "n": n, "snr": snr, "lbd": lbd,
+                   "rank": rank, "noise": np.round(np.log10(noise), 2), "mse_sig": mse_sig,
+                   "mse_rel": mse_rel, "pr_rho": pr_rho, "pr_pval": pr_pval, "gamma": gamma}
+            rows.append(row)
+
+        if len(rows) == len(methods):
+            writer.writerows(rows)
+            count += len(rows)
+            print("%s Written %d rows (n=%d)" % (str(datetime.datetime.now()), count, n))
 
 
 def test(n=100, noise=0.1, rank=10, lbd=0.1, seed=None, P=5, gmin=-1, gmax=4, delta=10, plot=True):
@@ -210,12 +213,4 @@ def test(n=100, noise=0.1, rank=10, lbd=0.1, seed=None, P=5, gmin=-1, gmax=4, de
 
 
 if __name__ == "__main__":
-    N = 100
-    R = np.zeros((N, 2))
-    for seed in range(N):
-        rho_Klist, rho_csi = test(lbd=0.0, rank=5, n=20, plot=False,
-                                  P=1, gmin=0, seed=seed, noise=0)
-        R[seed, 0] = rho_Klist
-        R[seed, 1] = rho_csi
-    print(np.sum(R[:, 0] > R[:, 1]))
-    plt.plot(R[:, 0], R[:, 1], "k.")
+    process()
