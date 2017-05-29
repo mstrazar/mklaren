219f49b7e697a2774d57576fac33c033bbee72e7
mstrazar <martin.strazar@gmail.com>
2017-05-29 22:39:29 +0200

    Fomralized inducing points selection experiment.

--- examples/snr.py
+++ examples/snr.py
@@ -23,7 +23,7 @@ import os
 import itertools as it
 
 import numpy as np
-from scipy.stats import multivariate_normal as mvn, pearsonr
+from scipy.stats import multivariate_normal as mvn, pearsonr, entropy
 from mklaren.kernel.kernel import exponential_kernel, kernel_sum
 from mklaren.kernel.kinterface import Kinterface
 from mklaren.mkl.mklaren import Mklaren
@@ -32,20 +32,19 @@ from sklearn.metrics import mean_squared_error as mse
 import matplotlib.pyplot as plt
 
 
-
-# Parameters
-gamma_range = 2**(np.linspace(-2, 2, 5))  # Arbitrary kernel hyperparameters
-delta = 10                                # Arbitrary look-ahead parameter
-
-# Objective experimentation;
-n_range = [100, 300, 500]                 # Vaste enough range of dataset sizes (which are full-rank)
-noise_range = np.logspace(-2, 2, 7)       # Range of noise levels
-repeats = range(10)                       # Number of repeats
-rank_percents = [0.05, 0.1, 0.15]         # Rank percentages given n
-methods = ["Mklaren", "CSI", "ICD"]
-lbd_range = [0, 0.1, 0.3, 1, 3, 10, 30, 100]  # Vast enough range, such that methods should be able to capture optimum somewhere
-
 def process():
+    # Parameters
+    gamma_range = 2 ** (np.linspace(-2, 2, 5))  # Arbitrary kernel hyperparameters
+    delta = 10  # Arbitrary look-ahead parameter
+
+    # Objective experimentation;
+    n_range = [100, 300, 500]  # Vaste enough range of dataset sizes (which are full-rank)
+    noise_range = np.logspace(-2, 2, 7)  # Range of noise levels
+    repeats = range(10)  # Number of repeats
+    rank_percents = [0.05, 0.1, 0.15]  # Rank percentages given n
+    methods = ["Mklaren", "CSI", "ICD"]
+    lbd_range = [0, 0.1, 0.3, 1, 3, 10, 30,
+                 100]  # Vast enough range, such that methods should be able to capture optimum somewhere
 
     # Create output directory
     d = datetime.datetime.now()
@@ -117,25 +116,20 @@ def process():
             print("%s Written %d rows (n=%d)" % (str(datetime.datetime.now()), count, n))
 
 
-def test(n=100, noise=0.1, rank=10, lbd=0.1, seed=None,
-         P=5, gmin=-1, gmax=4, delta=10, plot=True, inducing_mode="uniform"):
+
+def generate_data(n, rank, inducing_mode="uniform", noise=1, gamma_range=(0.1,), seed=None):
     """
-    Sample data from a Gaussian process and compare fits with the sum of kernels
-    versus list of kernels.
-    :param n: Number of data points.
-    :param noise:  Noise variance.
-    :param rank:  Approximation rank.
-    :param lbd: Regularization parameter.
+
+    :param n: Number od data points.
+    :param rank: Number of inducing points.
+    :param inducing_mode:   Biased or uniform distribution of data points.
+    :param noise: Noise variance.
+    :param gamma_range: Number of kernels and hyperparameters.
     :param seed: Random seed.
-    :param inducing_mode: How to choose inducing points.
     :return:
     """
-
     if seed is not None:
         np.random.seed(seed)
-
-    # small gamma, large lengthscale -> small gamma, small frequency
-    gamma_range = np.logspace(gmin, gmax, P)
 
     # Generate data
     X = np.linspace(-10, 10, n).reshape((n, 1))
@@ -143,138 +137,295 @@ def test(n=100, noise=0.1, rank=10, lbd=0.1, seed=None,
 
     # Kernel sum
     Ksum = Kinterface(data=X, kernel=kernel_sum,
-                   kernel_args={
-                       "kernels": [exponential_kernel] * len(gamma_range),
-                       "kernels_args": [{"gamma": g} for g in gamma_range]})
+                      kernel_args={
+                          "kernels": [exponential_kernel] * len(gamma_range),
+                          "kernels_args": [{"gamma": g} for g in gamma_range]})
 
     # Sum of kernels
     Klist = [Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": g})
-            for g in gamma_range]
+             for g in gamma_range]
 
-    a = np.arange(n, dtype=float)
+    a = np.arange(n, dtype=int)
     if inducing_mode == "uniform":
         p = None
     elif inducing_mode == "biased":
         p = (a ** 2 / (a ** 2).sum())
     else:
         raise ValueError(inducing_mode)
-    inxs = np.random.choice(a, p=p, size = rank, replace = False)
+
+    inxs = np.random.choice(a, p=p, size=rank, replace=False)
     Kny = Ksum[:, inxs].dot(np.linalg.inv(Ksum[inxs, inxs])).dot(Ksum[inxs, :])
     f = mvn.rvs(mean=np.zeros((n,)), cov=Kny)
-
-    # f = mvn.rvs(mean=np.zeros((n,)), cov=Ksum[:, :])
     y = mvn.rvs(mean=f, cov=noise * np.eye(n, n))
+
+    return Ksum, Klist, inxs, X, Xp, y, f
+
+
+# def plot_signal(X, Xp, y, f, models=(), tit=""):
+#     """
+#     Plot fitted signal.
+#
+#     :param X: Sampling coordinates.
+#     :param Xp:  Plotting (whole signal) coordinates.
+#     :param y:   True observed values.
+#     :param f:   True signal.
+#     :param models: Onr dictionary per model;
+#         "yp"    Predicted signal at yp.
+#         "anchors" Anchor (inducing points coordinates), one set per lengthscale.
+#         "color": Color.
+#         "label": Name.
+#     :param tit:
+#     :return:
+#     """
+#
+#     # Plot signal
+#     plt.figure()
+#     x = X.ravel()
+#     xp = Xp.ravel()
+#     plt.plot(x, y, "k.")
+#     plt.plot(x, f, "r--")
+#
+#     # Compute anchor ticks
+#     P = max([1] + map(lambda m: len(m.get("anchors", [])), models))
+#     ymin = int(np.absolute(np.min(y)))
+#     Gxs = [np.linspace(-10, 10, 5 + 10 * g) for g in np.logspace(-1, 1, P)]
+#     Gys = range(-ymin - len(Gxs), -ymin)
+#
+#     # Plot freqency scales
+#     for gi, (gx, gy) in enumerate(zip(Gxs, Gys)):
+#         plt.plot(gx, [gy] * len(gx), "|", color="gray")
+#
+#     # Plot multiple signals and anchors
+#     for data in models:
+#         label = data.get("label", "model")
+#         yp = data.get("yp", np.zeros((len(X), )))
+#         color = data.get("color", "blue")
+#         anchors = data.get("anchors", [[]])
+#         rho, _ = pearsonr(yp, f)
+#         plt.plot(xp, yp, "-", color=color, label="%s ($\\rho$=%.2f)" % (label, rho))
+#
+#         for gi in range(P):
+#             if not len(anchors[gi]): continue
+#             print("Number of pivots at gamma  %d: %d" % (gi, len(anchors_Klist[gi])))
+#             plt.plot(anchors[gi], [Gys[gi]] * len(anchors[gi]), "^", color=color, markersize=8, alpha=0.6)
+#
+#     plt.title(tit)
+#     ylim = plt.gca().get_ylim()
+#     plt.legend()
+#     plt.xlim((-x.min()-1, x.max()+1))
+#     plt.ylim((ylim[0]-1, ylim[1]))
+#     plt.show()
+
+
+def test(Ksum, Klist, inxs, X, Xp, y, f, delta=10, lbd=0.1):
+    """
+    Sample data from a Gaussian process and compare fits with the sum of kernels
+    versus list of kernels.
+    :param Ksum:
+    :param Klist:
+    :param inxs:
+    :param X:
+    :param Xp:
+    :param y:
+    :param f:
+    :param delta:
+    :param lbd:
+    :return:
+    """
+    P = len(Klist)                  # Number of kernels
+    rank = len(inxs)      # Total number of inducing points over all lengthscales
+    anchors = X[inxs,].ravel()
 
     # Fit MKL for kernel sum and
     mkl = Mklaren(rank=rank,
                   delta=delta, lbd=lbd)
     mkl.fit(Klist, y)
     y_Klist = mkl.predict([X] * len(Klist))
-    yp_Klist = mkl.predict([Xp] * len(Klist))
-    active_Klist = [mkl.data.get(gi, {}).get("act", [])for gi in range(P)]
-    anchors_Klist = [X[mkl.data.get(gi, {}).get("act", [])].ravel() for gi in range(P)]
-
+    # yp_Klist = mkl.predict([Xp] * len(Klist))
+    active_Klist = [mkl.data.get(gi, {}).get("act", []) for gi in range(P)]
+    anchors_Klist = [X[ix] for ix in active_Klist]
 
     mkl.fit([Ksum], y)
     y_Ksum = mkl.predict([X])
-    yp_Ksum = mkl.predict([Xp])
+    # yp_Ksum = mkl.predict([Xp])
 
     # Fit CSI
     csi = RidgeLowRank(rank=rank, lbd=lbd,
                        method="csi", method_init_args={"delta": delta},)
     csi.fit([Ksum], y)
     y_csi = csi.predict([X])
-    yp_csi = csi.predict([Xp])
+    # yp_csi = csi.predict([Xp])
     active_csi = [csi.active_set_[gi] for gi in range(P)]
-    anchors_csi = [X[csi.active_set_[gi]] for gi in range(P)]
+    anchors_csi = [X[ix] for ix in active_csi]
 
-    # Frequency scales
-    ymin = int(np.absolute(np.min(y)))
-    Gxs = [np.linspace(-10, 10, 5 + 10 * g) for g in gamma_range]
-    Gys = range(-ymin-len(Gxs), -ymin)
+    # Fit Nystrom
+    nystrom = RidgeLowRank(rank=rank, lbd=lbd,
+                       method="nystrom")
+    nystrom.fit([Ksum], y)
+    y_nystrom = nystrom.predict([X])
+    # yp_nystrom = nystrom.predict([Xp])
+    active_nystrom = [nystrom.active_set_[gi] for gi in range(P)]
+    anchors_nystrom = [X[ix] for ix in active_nystrom]
 
     # Correlation
     rho_Klist, _ = pearsonr(y_Klist, f)
     rho_Ksum, _ = pearsonr(y_Ksum, f)
     rho_csi, _ = pearsonr(y_csi, f)
+    rho_nystrom, _ = pearsonr(y_nystrom, f)
+
+    # Distance between anchors
+    idp_dist_Klist = inducing_points_distance(anchors, anchors_Klist)
+    idp_dist_CSI = inducing_points_distance(anchors, anchors_csi)
+    idp_dist_Nystrom = inducing_points_distance(anchors, anchors_nystrom)
 
     # Plot a summary figure
-    if plot:
-        plt.figure()
-
-        # Plot signal
-        x = X.ravel()
-        xp = Xp.ravel()
-        plt.plot(x, y, "k.")
-        plt.plot(x, f, "r--")
-        plt.plot(xp, yp_Klist, "g-", label="Klist ($\\rho$=%.2f)" % rho_Klist)
-        # plt.plot(xp, yp_Ksum, "b-", label="Ksum ($\\rho$=%.2f)" % rho_Ksum)
-        plt.plot(xp, yp_csi, "r-", label="CSI ($\\rho$=%.2f)" % rho_csi)
-
-        # Plot freqency scales
-        for gi, (gx, gy) in enumerate(zip(Gxs, Gys)):
-            plt.plot(gx, [gy] * len(gx), "|", color="gray")
-            if len(anchors_Klist[gi]):
-                print("Number of pivots at gamma  %d: %d" % (gi, len(anchors_Klist[gi])))
-                plt.plot(anchors_Klist[gi], [gy] * len(anchors_Klist[gi]), "^", color="green", markersize=8, alpha=0.6)
-            if len(anchors_csi[gi]):
-                plt.plot(anchors_csi[gi], [gy] * len(anchors_Klist[gi]), "^", color="red", markersize=8, alpha=0.6)
-        plt.title("n=%d, noise=%.3f, rank=%d, lambda=%0.3f" % (n, np.max(noise), rank, lbd))
-        ylim = plt.gca().get_ylim()
-        plt.legend()
-        plt.xlim((-11, 11))
-        plt.ylim((ylim[0]-1, ylim[1]))
-        plt.show()
-    else:
-        return inxs, rho_Klist, rho_csi, active_Klist, active_csi
+    return {"anchors": anchors,
+            "Mklaren": {
+                 "rho": rho_Klist,
+                 "active": active_Klist,
+                 "anchors": anchors_Klist,
+                 "idp": idp_dist_Klist},
+            "CSI": {
+                "rho": rho_csi,
+                "active": active_csi,
+                "anchors": anchors_csi,
+                "idp": idp_dist_CSI},
+            "Nystrom": {
+                "rho": rho_nystrom,
+                "active": active_nystrom,
+                "anchors": anchors_nystrom,
+                "idp": idp_dist_Nystrom}
+            }
 
 
-if __name__ == "__main__":
-    # process()
-    n = 100
-    noise = 1
-    rank = 3
-    lbd = 0
+
+def hist_total_variation(h1, h2):
+    """
+    Total variation between two histograms, assuming identical bins.
+    :param h1: Histogram.
+    :param h2: Histogram.
+    :return: Total variation distance.
+    """
+    return np.sum(np.absolute(h1 - h2))
+
+
+def inducing_points_distance(A, B):
+    """
+    Compute distance betwen two sets of inducing points)
+    :param A: Set of inducing points (in any dimension), given as coordinates.
+        Inducing points can be defined in terms of the training set or pseudo-inducing points.
+    :param B: Same as A.
+    :return: Minimal distance given permutations of x.
+    """
+    A = np.array(A)
+    B = np.array(B)
+    inxs = range(len(B))
+    return min((np.linalg.norm(A - B[list(ix)]) for ix in it.permutations(inxs)))
+
+
+
+def main():
+    # Experiment paramaters
+    n_range = (100, 300 ,1000)
+    rank_range = (3, 5, 10)
+    lbd_range = (0, 0.1, 1)
+    gamma_range = np.logspace(-1, 1, 3)
     repeats = 500
-
-    out_dir = "examples/output/snr/images/"
-
     noise_models = ("fixed", "increasing")
     sampling_models = ("uniform", "biased")
+    methods = ("Mklaren", "CSI", "Nystrom")
 
-    for noise_model, inducing_mode in it.product(noise_models, sampling_models):
+    # Create output directory
+    d = datetime.datetime.now()
+    dname = os.path.join("output", "snr", "%d-%d-%d" % (d.year, d.month, d.day))
+    if not os.path.exists(dname):
+        os.makedirs(dname)
+    subdname = os.path.join(dname, "details_%d" % len(os.listdir(dname)))
+    if not os.path.exists(subdname):
+        os.makedirs(subdname)
+    fname = os.path.join(dname, "results_%d.csv" % len(os.listdir(dname)))
+    print("Writing to %s ..." % fname)
+
+    # Output file
+    header = ["method", "noise.model", "sampling.model", "n", "rank", "lbd", "gamma",
+              "anchors.dist", "anchors.dist.sd", "total.variation", "kl.divergence"]
+    writer = csv.DictWriter(open(fname, "w", buffering=0), fieldnames=header)
+    writer.writeheader()
+
+    count = 0
+    for noise_model, inducing_mode, rank, lbd, gamma, n in it.product(noise_models,
+                                                         sampling_models,
+                                                         rank_range,
+                                                         lbd_range,
+                                                         gamma_range,
+                                                         n_range):
         if noise_model == "fixed":
             noise = 1
         else:
             noise = np.logspace(-2, 2, n)
 
+        avg_actives = dict()
         avg_anchors = dict()
+        avg_dists = dict()
 
         for seed in range(repeats):
-            r = test(n=n, P=1, noise=noise, gmin=0, gmax=1, rank=rank, seed=seed, lbd=lbd,
-                     plot=False, inducing_mode=inducing_mode)
-            inxs, rho_Klist, rho_csi, active_Klist, active_csi = r
-            avg_anchors["Mklaren"] = avg_anchors.get("Mklaren", []) + active_Klist[0]
-            avg_anchors["CSI"] = avg_anchors.get("CSI", []) + active_csi[0]
-            avg_anchors["True"] = avg_anchors.get("True", []) + list(inxs)
+            # Generate data
+            Ksum, Klist, inxs, X, Xp, y, f = generate_data(n=n,
+                                                           rank=rank,
+                                                           inducing_mode=inducing_mode,
+                                                           noise=noise,
+                                                           gamma_range=[gamma],
+                                                           seed=seed)
+            # Evaluate methods
+            try:
+                r = test(Ksum, Klist, inxs, X, Xp, y, f)
+            except Exception as e:
+                print("Exception, continuing: %s" % str(e))
+                continue
+
+            # Fill in anchors and active points
+            avg_actives["True"] = avg_actives.get("True", []) + list(inxs)
+            avg_anchors["True"] = avg_anchors.get("True", []) + list(r["anchors"])
+            for m in methods:
+                avg_actives[m] = avg_actives.get(m, []) + list(r[m]["active"][0])
+                avg_anchors[m] = avg_anchors.get(m, []) + list(r[m]["anchors"][0])
+                avg_dists[m] = avg_dists.get(m, []) + r[m]["idp"]
 
         # Compare distributions
-        bins =  np.array([  4. ,  13.5,  23. ,  32.5,  42. ,  51.5,  61. ,  70.5,  80. , 89.5,  99. ])
-        avg_mkl, _ = np.histogram(avg_anchors["Mklaren"], normed=True, bins=bins)
-        avg_csi, _ = np.histogram(avg_anchors["CSI"], normed=True, bins=bins)
-        avg_tru, _ = np.histogram(avg_anchors["True"], normed=True, bins=bins)
-        mkl_tot_var = np.sum(np.absolute(avg_mkl - avg_tru))
-        csi_tot_var = np.sum(np.absolute(avg_csi - avg_tru))
+        rows = []
+        probs, bins  = np.histogram(avg_actives["True"], normed=True)
+        for m in methods:
+            query, _ = np.histogram(avg_actives[m], normed=True, bins=bins)
+            kl = entropy(probs, query)
+            tv = hist_total_variation(probs, query)
+            idp_mean = np.mean(avg_dists[m])
+            idp_std = np.std(avg_dists[m])
+            row  = {"method": m, "noise.model": noise_model, "sampling.model": inducing_mode,
+                    "n": n, "rank":rank, "lbd": lbd, "gamma": gamma,
+                    "anchors.dist": idp_mean, "anchors.dist.sd": idp_std,
+                    "total.variation": tv, "kl.divergence": kl}
+            rows.append(row)
+        writer.writerows(rows)
+
+        count += len(rows)
+        print("%s Written %d rows"% (str(datetime.datetime.now()), count))
+
 
         # Plot histograms
-        fname = os.path.join(out_dir, "noisy_sampling_%s-%s.pdf" % (noise_model, inducing_mode))
-        fig, ax = plt.subplots(nrows=3, ncols=1)
-        ax[0].hist(avg_anchors["True"], color="gray", label="True")
-        ax[1].hist(avg_anchors["Mklaren"], color="green", label="Mklaren (%.3f)" % mkl_tot_var)
-        ax[2].hist(avg_anchors["CSI"], color="blue", label="CSI (%.3f)" % csi_tot_var)
+        figname = os.path.join(subdname, "noisy_sampling_%s_%s_n-%d_rank-%d_lbd-%.3f_gamma-%.3f.pdf"
+                             % (noise_model, inducing_mode, n, rank, lbd, gamma))
+        fig, ax = plt.subplots(nrows=4, ncols=1)
+        ax[0].hist(avg_actives["True"], color="gray", label="True", bins=bins)
+        ax[1].hist(avg_actives["Mklaren"], color="green", label="Mklaren", bins=bins)
+        ax[2].hist(avg_actives["CSI"], color="blue", label="CSI", bins=bins)
+        ax[3].hist(avg_actives["Nystrom"], color="red", label="Nystrom", bins=bins)
         for i in range(len(ax)):
             ax[i].legend()
             ax[i].set_xlim((0, n))
-        plt.savefig(fname)
+        plt.savefig(figname)
         plt.close()
-        print("Written %s" % fname)
+        print("Written %s" % figname)
+
+
+if __name__ == "__main__":
+    main()
