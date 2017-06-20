b5ec4b029c7ac75284c090bb3e1b748d3bc6a1f8
mstrazar <martin.strazar@gmail.com>
2017-06-20 13:50:34 +0200

    Plot latest results.

--- examples/time_series/time_series.R
+++ examples/time_series/time_series.R
@@ -4,10 +4,9 @@ setwd("/Users/martin/Dev/mklaren/examples/time_series/")
 # n = 1000 ; Add FITC
 data = read.csv("../output/energy/2017-6-15/results_0.csv", header = TRUE)
 
+data = read.csv("../output/energy/2017-6-20/results_2.csv", header = TRUE)
+
+# Validation error
 x11()
 qplot(data=data, x=as.factor(signal), y=mse_y, fill=method, geom="boxplot",
-      xlab="Time series", ylab="MSE")
-
-# Remember that we dont know the true function 
-qplot(data=data, x=as.factor(signal), y=mse_f, fill=method, geom="boxplot",
-      xlab="Time series", ylab="MSE (f)")
+      xlab="Time series", ylab="MSE", ylim=c(0, 15))
