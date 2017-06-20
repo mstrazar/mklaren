7a53c476965cab71f555045cfeb75dabbe207fbc
mstrazar <martin.strazar@gmail.com>
2017-06-20 12:46:43 +0200

    Backup old results on fitting error.

--- examples/time_series/time_series.R
+++ examples/time_series/time_series.R
@@ -1,11 +1,8 @@
 require(ggplot2)
 setwd("/Users/martin/Dev/mklaren/examples/time_series/")
 
-# n = 30
-data = read.csv("../output/energy/2017-6-8/results_0.csv", header = TRUE)
-
-# n = 1000
-data = read.csv("../output/energy/2017-6-8/results_2.csv", header = TRUE)
+# n = 1000 ; Add FITC
+data = read.csv("../output/energy/2017-6-15/results_0.csv", header = TRUE)
 
 x11()
 qplot(data=data, x=as.factor(signal), y=mse_y, fill=method, geom="boxplot",
