8deed67668459a4dfa784c1ae5c2b2a74ee2598a
mstrazar <martin.strazar@gmail.com>
2017-06-21 16:36:54 +0200

    Plot for cross-validation.

--- examples/time_series/time_series.R
+++ examples/time_series/time_series.R
@@ -4,9 +4,26 @@ setwd("/Users/martin/Dev/mklaren/examples/time_series/")
 # n = 1000 ; Add FITC
 data = read.csv("../output/energy/2017-6-15/results_0.csv", header = TRUE)
 
+# n = 500/500 ; Add RFF
 data = read.csv("../output/energy/2017-6-20/results_2.csv", header = TRUE)
 
-# Validation error
-x11()
-qplot(data=data, x=as.factor(signal), y=mse_y, fill=method, geom="boxplot",
-      xlab="Time series", ylab="MSE", ylim=c(0, 15))
+# Results for CV for different values of lambda and ranks
+alldata = read.csv("../output/energy/2017-6-20/results_4.csv", header = TRUE)
+alldata$name = sprintf("%s.%s.%s.%s", alldata$method, alldata$tsi, 
+                                      alldata$signal, alldata$rank)
+    # Select rows via CV
+    af = aggregate(alldata$mse_val, by=list(method=alldata$method, 
+                                    tsi=alldata$tsi, 
+                                    signal=alldata$signal,
+                                    rank=alldata$rank), min)
+    row.names(af) <- sprintf("%s.%s.%s.%s", af$method, af$tsi, af$signal, af$rank)
+    alldata$best = af[alldata$name, "x"] == alldata$mse_val
+    data = alldata[alldata$best,]
+
+# Validation error per each rank
+for (r in unique(data$rank)){
+  qplot(data=data[data$rank == r,], x=as.factor(signal), 
+        y=mse_y, fill=method, geom="boxplot",
+        xlab="Time series", ylab="MSE", ylim=c(0, 15), 
+        main=sprintf("Rank=%d", r))  
+}
