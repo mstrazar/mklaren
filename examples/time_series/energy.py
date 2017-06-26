46a3562a4fefcbea5f4ac5e8d74004bbfe6966d4
mstrazar <martin.strazar@gmail.com>
2017-06-26 15:03:07 +0200

    Simplify code and run in parallel.

--- examples/time_series/energy.py
+++ examples/time_series/energy.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+hlp = """
+    Test prediction on the energy dataset with matern, exponential and
+    periodic kernels.
+
+    Usage: ./energy.py experiment sig
+
+    experiment: [exponential] matern periodic
+    sig: [T1]-T9
+"""
 if __name__ == "__main__":
     import matplotlib
     matplotlib.use("Agg")
@@ -5,14 +15,12 @@ if __name__ == "__main__":
 import csv
 import datetime
 import os
-
 import sys
 import numpy as np
 import itertools as it
 from mklaren.kernel.kernel import exponential_kernel, kernel_sum, matern32_gpy, periodic_kernel
 from mklaren.kernel.kinterface import Kinterface
 from sklearn.metrics import mean_squared_error as mse
-
 from datasets.energy import load_energy
 from examples.snr.snr import plot_signal, test
 
@@ -21,6 +29,7 @@ from examples.snr.snr import plot_signal, test
 # https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction#
 args = dict(enumerate(sys.argv))
 ename = args.get(1, "exponential")
+sig = args.get(2, "T1")
 print("Proceeding with %s scenario ..." % ename)
 
 kernel_function = None
@@ -52,6 +61,7 @@ lambda_range = [0] + list(np.logspace(-1, 1, 5))
 
 # Data parameters
 signals = ["T%d" % i for i in range(1, 10)]
+assert sig in signals
 inxs = range(1, 19)
 
 # Store results
@@ -69,74 +79,72 @@ header = ["experiment", "signal",  "tsi", "method",
 writer = csv.DictWriter(open(fname, "w", buffering=0), fieldnames=header)
 writer.writeheader()
 
-for sig in signals:
 
-    # Load energy condumption data
-    data = load_energy(signal=sig)
-    data_out = load_energy(signal="T_out")
-    X, labels = data["data"], data["labels"]
-    X_out, _ = data_out["data"], data_out["labels"]
+# Load energy consumption data
+data = load_energy(signal=sig)
+data_out = load_energy(signal="T_out")
+X, labels = data["data"], data["labels"]
+X_out, _ = data_out["data"], data_out["labels"]
 
-    # Normalize by outside temperature
-    # Assume true signal is the mean of the signals
-    Y = X - X_out
-    N, n = Y.shape
+# Normalize by outside temperature
+# Assume true signal is the mean of the signals
+Y = X - X_out
+N, n = Y.shape
 
-    # Training, validation, test
-    if ename == "periodic":
-        x = np.atleast_2d(np.arange(0, n/2)).T
-        xv = np.atleast_2d(np.arange(n/2, n, 2)).T
-        xp = np.atleast_2d(np.arange(n/2+1, n, 2)).T
-    else:
-        x  = np.atleast_2d(np.arange(0, n, 3)).T
-        xv = np.atleast_2d(np.arange(1, n, 3)).T
-        xp = np.atleast_2d(np.arange(2, n, 3)).T
+# Training, validation, test
+if ename == "periodic":
+    x = np.atleast_2d(np.arange(0, n/2)).T
+    xv = np.atleast_2d(np.arange(n/2, n, 2)).T
+    xp = np.atleast_2d(np.arange(n/2+1, n, 2)).T
+else:
+    x  = np.atleast_2d(np.arange(0, n, 3)).T
+    xv = np.atleast_2d(np.arange(1, n, 3)).T
+    xp = np.atleast_2d(np.arange(2, n, 3)).T
 
-    Nt, Nv, Np = x.shape[0], xv.shape[0], xp.shape[0]
-    f = Y[1:19, x].mean(axis=0).ravel()
+Nt, Nv, Np = x.shape[0], xv.shape[0], xp.shape[0]
+f = Y[1:19, x].mean(axis=0).ravel()
 
-    for rank, lbd in it.product(rank_range, lambda_range):
-        for tsi in inxs:
-            y = Y[tsi, x].reshape((Nt, 1))
-            yv = Y[tsi, xv].reshape((Nv, 1))
-            yp = Y[tsi, xp].reshape((Np, 1))
+for rank, lbd, tsi in it.product(rank_range, lambda_range, inxs):
+    y = Y[tsi, x].reshape((Nt, 1))
+    yv = Y[tsi, xv].reshape((Nv, 1))
+    yp = Y[tsi, xp].reshape((Np, 1))
 
-            # Sum and List of kernels
-            vals = list(it.product(*pars.values()))
-            names = pars.keys()
-            Klist = [Kinterface(data=x, kernel=kernel_function, row_normalize=True,
-                                kernel_args=dict([(n, v) for n, v in zip(names, vlist)])) for vlist in vals]
-            Ksum = Kinterface(data=x, kernel=kernel_sum, row_normalize=True,
-                                kernel_args={"kernels": [kernel_function] * len(vals),
-                                             "kernels_args": [dict([(n, v) for n, v in zip(names, vlist)])
-                                                              for vlist in vals]})
-            # Fit models and plot signal
-            # Remove True anchors, as they are non-existent
-            try:
-                models_val = test(Ksum=Ksum, Klist=Klist,
-                              inxs=range(rank),
-                              X=x, Xp=xv, y=y, f=f,  delta=delta, lbd=lbd,
-                              methods=methods)
-                models = test(Ksum=Ksum, Klist=Klist,
-                              inxs=range(rank),
-                              X=x, Xp=xp, y=y, f=f, delta=delta, lbd=lbd,
-                              methods=methods)
-            except:
-                continue
-            del models["True"]
+    # Sum and List of kernels
+    vals = list(it.product(*pars.values()))
+    names = pars.keys()
+    Klist = [Kinterface(data=x, kernel=kernel_function, row_normalize=True,
+                        kernel_args=dict([(n, v) for n, v in zip(names, vlist)])) for vlist in vals]
+    Ksum = Kinterface(data=x, kernel=kernel_sum, row_normalize=True,
+                        kernel_args={"kernels": [kernel_function] * len(vals),
+                                     "kernels_args": [dict([(n, v) for n, v in zip(names, vlist)])
+                                                      for vlist in vals]})
+    # Fit models and plot signal
+    # Remove True anchors, as they are non-existent
+    try:
+        models_val = test(Ksum=Ksum, Klist=Klist,
+                      inxs=range(rank),
+                      X=x, Xp=xv, y=y, f=f,  delta=delta, lbd=lbd,
+                      methods=methods)
+        models = test(Ksum=Ksum, Klist=Klist,
+                      inxs=range(rank),
+                      X=x, Xp=xp, y=y, f=f, delta=delta, lbd=lbd,
+                      methods=methods)
+    except:
+        continue
+    del models["True"]
 
-            # Store file
-            fname = os.path.join(subdname, "plot_sig-%s_tsi-%d_lbd-%.3f_rank-%d.pdf" % (sig, tsi, lbd, rank))
-            plot_signal(X=xp, Xp=xp, y=yp, f=yp, models=models, f_out=fname,
-                        typ="plot_models")
+    # Store file
+    fname = os.path.join(subdname, "plot_sig-%s_tsi-%d_lbd-%.3f_rank-%d.pdf" % (sig, tsi, lbd, rank))
+    plot_signal(X=xp, Xp=xp, y=yp, f=yp, models=models, f_out=fname,
+                typ="plot_models")
 
-            for ky in models.keys():
-                mse_yv = mse(models_val[ky]["yp"].ravel(), yv.ravel())
-                mse_yp = mse(models[ky]["yp"].ravel(), yp.ravel())
+    for ky in models.keys():
+        mse_yv = mse(models_val[ky]["yp"].ravel(), yv.ravel())
+        mse_yp = mse(models[ky]["yp"].ravel(), yp.ravel())
 
-                time = models[ky]["time"]
-                row = {"experiment": ename,
-                       "signal": sig,  "tsi": tsi, "method": ky,
-                       "mse_f": 0, "mse_y": mse_yp, "mse_val": mse_yv,
-                       "rank": rank, "lbd": lbd, "n": n, "time": time}
-                writer.writerow(row)
+        time = models[ky]["time"]
+        row = {"experiment": ename,
+               "signal": sig,  "tsi": tsi, "method": ky,
+               "mse_f": 0, "mse_y": mse_yp, "mse_val": mse_yv,
+               "rank": rank, "lbd": lbd, "n": n, "time": time}
+        writer.writerow(row)
