ca1ed9b61675aeea14d1e7c13619e61a81fb9a63
mstrazar <martin.strazar@gmail.com>
2017-06-27 16:12:52 +0200

    Batch run of blitzer sentiment 2.

---
+++ examples/blitzer/blitzer_sentiment2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Run blitzer_sentiment for L2KRR; Features are still pre-filtered.
+export PYTHONPATH=../..
+export MKL_NUM_THREADS=5
+export NUMEXPR_NUM_THREADS=5
+export OMP_NUM_THREADS=5
+
+for dset in books dvd electronics kitchen ; do
+    echo python blitzer_sentiment2.py $dset
+    python blitzer_sentiment2.py $dset 2>$dset.2.err.txt 1>$dset.2.out.txt &
+    sleep 1
+done
+
+echo "Waiting..."
+wait
+echo "End."
