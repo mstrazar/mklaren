c4b1b1c691b2eb6af9b1c4fb4800227083117ac5
mstrazar <martin.strazar@gmail.com>
2017-06-27 14:55:14 +0200

    Add L2 kernel ridge regression.

--- examples/blitzer/blitzer_sentiment.py
+++ examples/blitzer/blitzer_sentiment.py
@@ -1,4 +1,4 @@
-import examples.__config__
+import sys
 
 # Methods
 from mklaren.mkl.mklaren import Mklaren
@@ -18,7 +18,8 @@ from numpy import logspace, array, vstack, log10, argsort, var
 from random import shuffle, seed
 
 # Dataset and number of samples
-dset    = "books" if len(argv) == 1 else argv[1]
+argv = dict(enumerate(sys.argv))
+dset    = argv.get(1, "books")
 n       = None
 max_features = 500
 delta   = 1     # Number of look-ahead columns
@@ -35,7 +36,9 @@ methods = {
     "uniform": (RidgeMKL, {"method": "uniform", "low_rank": True}),
     "align":   (RidgeMKL, {"method": "align", "low_rank": True}),
     "alignf":  (RidgeMKL, {"method": "alignf", "low_rank": True}),
-    "alignfc": (RidgeMKL, {"method": "alignfc", "low_rank": True})
+    "alignfc": (RidgeMKL, {"method": "alignfc", "low_rank": True}),
+    "l2krr":   (RidgeMKL, {"method": "alignfc", "low_rank": True})
+
 }
 
 
@@ -86,67 +89,66 @@ if center:
     Vs = map(center_kernel_low_rank, Vs)
 
 # Set rank range wrt. training size
-rank_range = [10, 20, 40, 80, 160, ]
+rank_range = [10, 20, 40, 80, 160, 320][:1]
 
 for cv in range(cv_iter):
-        seed(cv)
+    seed(cv)
 
-        # Split into training, validation, test sets.
-        shuffle(inxs_tr)
-        n1 = int(training_size   * len(inxs_tr))
+    # Split into training, validation, test sets.
+    shuffle(inxs_tr)
+    n1 = int(training_size   * len(inxs_tr))
 
-        tr,   tval,    te = inxs_tr[:n1], inxs_tr[n1:], inxs_te
-        y_tr, y_val, y_te = y[tr], y[tval], y[te]
+    tr,   tval,    te = inxs_tr[:n1], inxs_tr[n1:], inxs_te
+    y_tr, y_val, y_te = y[tr], y[tval], y[te]
 
-        V_tr  = [Kinterface(data=v[tr], kernel=linear_kernel) for v in Vs]
-        V_val = [v[tval] for v in Vs]
-        V_te  = [v[te] for v in Vs]
+    Ks_tr  = [Kinterface(data=v[tr], kernel=linear_kernel) for v in Vs]
+    V_val = [v[tval] for v in Vs]
+    V_te  = [v[te] for v in Vs]
 
+    output = "\t".join(["Method", "effective rank", "iteration", "lambda", "RMSE"])
+    print(output)
 
-        output = "\t".join(["Method", "effective rank", "iteration", "lambda", "RMSE"])
-        print(output)
+    for rank in rank_range:
 
-        for rank in rank_range:
+        # Select a subset of kernels for full-rank methods ; use align
+        premodel = AlignLowRank()
+        premodel.fit(Vs, y, holdout = tval + te)
+        kernel_order = argsort(premodel.mu.ravel())[::-1]
+        Vs_subset = [Vs[kernel_order[i]] for i in range(rank)]
 
-            # Select a subset of kernels for full-rank methods ; use align
-            premodel = AlignLowRank()
-            premodel.fit(Vs, y, holdout = tval + te)
-            kernel_order = argsort(premodel.mu.ravel())[::-1]
-            Vs_subset = [Vs[kernel_order[i]] for i in range(rank)]
+        # Fit hyperparameters on the validation set using the current rank
+        for mname, (Mclass, kwargs) in methods.items():
 
-            # Fit hyperparameters on the validation set using the current rank
-            for mname, (Mclass, kwargs) in methods.items():
+            best_lambda = lbd_range[0]
+            best_score  = float("inf")
+            if mname == "Mklaren":
+                kwargs["rank"] = rank
 
-                best_lambda = lbd_range[0]
-                best_score  = float("inf")
+            for lbd in lbd_range:
+                model = Mclass(lbd=lbd, **kwargs)
                 if mname == "Mklaren":
-                    kwargs["rank"] = rank
-
-                for lbd in lbd_range:
-                    model = Mclass(lbd=lbd, **kwargs)
-                    if mname == "Mklaren":
-                        model.fit(V_tr, y_tr)
-                        yp    = model.predict(V_val).ravel()
-                    else:
-                        holdout = tval + te
-                        model.fit(Vs_subset, y, holdout=holdout)
-                        yp    = model.predict(tval).ravel()
-                    score = var(y_val.ravel() - yp)**0.5
-                    if score < best_score:
-                        best_score = score
-                        best_lambda = lbd
-
-                # Fit model with best hyperparameters
-                model = Mclass(lbd=best_lambda, **kwargs)
-                if mname == "Mklaren":
-                    model.fit(V_tr, y_tr)
-                    yp    = model.predict(V_te).ravel()
+                    model.fit(Ks_tr, y_tr)
+                    yp    = model.predict(V_val).ravel()
                 else:
                     holdout = tval + te
                     model.fit(Vs_subset, y, holdout=holdout)
-                    yp    = model.predict(te).ravel()
+                    yp    = model.predict(tval).ravel()
+                score = var(y_val.ravel() - yp)**0.5
+                if score < best_score:
+                    best_score = score
+                    best_lambda = lbd
 
-                score = var(y_te.ravel() - yp)**0.5
-                output = "\t".join(map(str, [mname, rank, cv, \
-                                             best_lambda, score]))
-                print(output)
+            # Fit model with best hyperparameters
+            model = Mclass(lbd=best_lambda, **kwargs)
+            if mname == "Mklaren":
+                model.fit(Ks_tr, y_tr)
+                yp    = model.predict(V_te).ravel()
+            else:
+                holdout = tval + te
+                model.fit(Vs_subset, y, holdout=holdout)
+                yp    = model.predict(te).ravel()
+
+            score = var(y_te.ravel() - yp)**0.5
+            output = "\t".join(map(str, [mname, rank, cv, \
+                                         best_lambda, score]))
+            print(output)
