71e23d991cf11c452b16958d129579c3be2995e2
mstrazar <martin.strazar@gmail.com>
2017-07-02 12:09:35 +0200

    Add more datasets to running script.

--- examples/delve/delve_regression2.sh
+++ examples/delve/delve_regression2.sh
@@ -6,7 +6,7 @@ export MKL_NUM_THREADS=5
 export NUMEXPR_NUM_THREADS=5
 export OMP_NUM_THREADS=5
 
-for dset in boston abalone comp bank pumadyn kin ; do
+for dset in boston abalone comp bank pumadyn kin ionosphere census ; do
     echo python delve_regression2.py $dset
     python delve_regression2.py $dset 2>$dset.2.err.txt 1>$dset.2.out.txt &
     sleep 1
