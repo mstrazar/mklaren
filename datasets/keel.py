f7636f0dae0bf1e12db209641b8a2fbbd3b8f0c1
mstrazar <martin.strazar@gmail.com>
2017-07-10 16:09:04 +0200

    Loading keel datasets.

---
+++ datasets/keel.py
@@ -0,0 +1,26 @@
+import gzip
+from numpy import genfromtxt
+from os.path import join, realpath, dirname
+
+KEEL_PATH = join(dirname(realpath(__file__)), "keel")
+
+def load_keel(name="abalone", n=None):
+    """
+    Load an keel dataset.
+    :param name: Dataset name.
+    :param n: Maximum number of examples.
+    :return: Dataset in standard form.
+    """
+    fp = gzip.open(join(KEEL_PATH, name, "%s.dat.gz" % name))
+    data = genfromtxt(fp, delimiter=",", skip_header=1, dtype=float, comments="@")
+    X = data[:, :-1]
+    y = data[:, -1].ravel()
+    labels = map(lambda i: "a%d" % i, range(X.shape[1]))
+
+    if n is not None and n < X.shape[0]:
+        X = X[:n, :]
+        y = y[:n]
+
+    return {
+        "data": X, "target": y, "labels": labels
+    }
