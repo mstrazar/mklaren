ce09bc8cd4248daf5fc2df3b3ddcbedd8c369b20
mstrazar <martin.strazar@gmail.com>
2017-07-31 16:03:23 +0200

    Renaming and cleanup.

--- examples/delve/delve_regression2_rank.R
+++ examples/delve/delve_regression2_rank.R
@@ -2,120 +2,68 @@ require(scmamp)
 require(xtable)
 setwd("/Users/martin/Dev/mklaren/examples/delve/")
 
-# testing kernels in range 1, 3, 5, ..., 100
-# in_dir = "2017-7-2"
-# inxs = seq(0, 7)
-
-# more resolution in rank computation
-# in_dir = "2017-7-3"
-# inxs = seq(0, 7)
-
-# more resolution in rank computation ; keel datasets
-in_dir = "2017-7-10"
-inxs = seq(1, 28) # 0 (ANACALT) is faulty;
-
 # Relevant files
+in_dir = "2017-7-28"
 in_files = Sys.glob(sprintf(file.path("../output/delve_regression/", in_dir, "/results_*.csv")))
 
-# Range of the number of kernels (temporary)
-p.range = c(0, 1, 2, 3, 5, 10)
-rnk.checks = c(5, 10, 30)
-
 # Aggregate results
-# meths = c("CSI", "ICD", "Mklaren", "Nystrom",  "RFF", "FITC", "Mklaren2")
-meths = c("CSI", "ICD", "Mklaren", "Nystrom",  "RFF")
-# datasets = c("boston", "abalone", "comp", "bank", 
-#              "pumadyn", "kin", "ionosphere", "census")
+meths = c("CSI", "ICD", "Mklaren", "Nystrom",  "RFF", "FITC")
 datasets = basename(Sys.glob("/Users/martin/Dev/mklaren/datasets/keel/*"))
-R = matrix(0, nrow=length(datasets), ncol=length(meths))
+R = matrix(NA, nrow=length(datasets), ncol=length(meths))
 row.names(R) = datasets
 colnames(R) = meths
 
-# List of summaries
-S.vec = vector(mode="list", length=length(rnk.checks)) 
-names(S.vec) = as.character(rnk.checks)
-  
-for(p in p.range){
-  R[,] = -1
-  for (in_file in in_files){
-    print(in_file)
-    alldata = read.csv(in_file, header=TRUE, stringsAsFactors = FALSE)
-    if(p != 0) alldata = alldata[alldata$p == p,]
-    alldata = alldata[alldata$method == "L2KRR" | (alldata$method %in% meths),]
-    alldata$RMSE_tr[is.na(alldata$RMSE_tr)] = Inf
-    alldata$RMSE_va[is.na(alldata$RMSE_va)] = Inf
-    alldata$RMSE[is.na(alldata$RMSE)] = Inf
-    dataset = unique(alldata$dataset)
-    
-    # Select lambda via cross-validation
-    agg = aggregate(alldata$RMSE_va, by=list(dataset=alldata$dataset,
-                                             method=alldata$method,
-                                             rank=alldata$rank,
-                                             iteration=alldata$iteration,
-                                             p=alldata$p), min)
-    row.names(agg) <- sprintf("%s.%s.%d.%s.%d", agg$dataset, agg$method, agg$rank, agg$iteration, agg$p)
-    inxs = sprintf("%s.%s.%d.%s.%d", alldata$dataset, alldata$method, alldata$rank, alldata$iteration, alldata$p)
-    alldata$best = alldata$RMSE_va == agg[inxs, "x"]
-    data = alldata[alldata$best,]
-    
-    # Aggregate mean and sd per rank
-    agg.m = aggregate(data[, "RMSE"], by=list(method=data[, "method"], rank=data[, "rank"]), mean)
-    agg.s = aggregate(data[, "RMSE"], by=list(method=data[, "method"], rank=data[, "rank"]), sd)
-    
-    # Print in a table at arbitrary checkpoints
-    for (r in rnk.checks){
-      if(is.null(S.vec[[as.character(r)]])){
-        S = matrix(0, nrow=length(datasets), ncol=length(meths))
-        row.names(S) = datasets
-        colnames(S) = meths
-        S.vec[[as.character(r)]] = S
-      } else {
-        S = S.vec[[as.character(r)]]
-      }
-      m = agg.m[agg.m$rank == r,]
-      s = agg.s[agg.s$rank == r,]
-      # S[dataset, m$method] = sprintf("%1.2E$\\pm$%1.2E", m$x, s$x)
-      S[dataset, m$method] = sprintf("%1.2E", m$x)
-      mm = m$method[which.min(m$x)]
-      S[dataset, mm] = sprintf("\\textbf{%s}", S[dataset, mm])
-      S.vec[[as.character(r)]] = S
-    }
-    
-    # Aggregate mean and sd per rank
-    t = agg.m[agg.m$method == "L2KRR", "x"] + agg.s[agg.s$method == "L2KRR", "x"]  
-    agg.valid = agg.m[agg.m$x < t,]
-    agg.min = aggregate(agg.valid$rank, by=list(method=agg.valid$method), min) 
-    row.names(agg.min) <- agg.min$method 
-    R[dataset,meths] = agg.min[meths, "x"]
-  }
-  
-  # Store checkpoints
-  for (r in rnk.checks){
-    S = S.vec[[as.character(r)]]
-    fname = sprintf("../output/delve_regression/tex/rank_table_p-%d_rank-%d.tex", p, r)
-    sink(fname)
-    print(xtable(S, caption = sprintf("p=%d K=%d", p, r)), 
-          sanitize.colnames.function=identity, 
-          sanitize.text.function=identity)
-    sink()
-    message(sprintf("Written %s", fname))
-  }
-  
-  # Rank
-  R[is.na(R)] = 85
-  rnk = apply(R, MARGIN = 1, FUN=rank)
-  rm = sort(rowMeans(rnk))
-  print(rm)
-  
-  # Store ranks
-  fname = sprintf("../output/delve_regression/rank_table_%d.tab", p)
-  write.table(R, fname, row.names = TRUE, sep = "\t")
-  message(sprintf("Written %s", fname))
-  
-  # Store CD
-  fname = sprintf("../output/delve_regression/rank_CD_%d.pdf", p)
-  pdf(fname)
-  plotCD(t(-rnk), alpha = 0.1)
-  dev.off()
-  message(sprintf("Written %s", fname))
+# Bind rows
+alldata = data.frame()
+for (in_file in in_files){
+  data = read.csv(in_file, header=TRUE, stringsAsFactors = FALSE)
+  alldata = rbind(alldata, data)
 }
+
+# Input checks 
+alldata = alldata[alldata$method == "L2KRR" | (alldata$method %in% meths),]
+alldata$RMSE_tr[is.na(alldata$RMSE_tr)] = Inf
+alldata$RMSE_va[is.na(alldata$RMSE_va)] = Inf
+alldata$RMSE[is.na(alldata$RMSE)] = Inf
+
+# Select lambda via cross-validation
+agg = aggregate(alldata$RMSE_va, by=list(dataset=alldata$dataset,
+                                         method=alldata$method,
+                                         rank=alldata$rank,
+                                         iteration=alldata$iteration,
+                                         p=alldata$p), min)
+row.names(agg) <- sprintf("%s.%s.%d.%s.%d", agg$dataset, agg$method, agg$rank, agg$iteration, agg$p)
+inxs = sprintf("%s.%s.%d.%s.%d", alldata$dataset, alldata$method, 
+               alldata$rank, alldata$iteration, alldata$p)
+alldata$best = alldata$RMSE_va == agg[inxs, "x"]
+data = alldata[alldata$best,]
+
+# Aggregate mean and sd per rank
+agg.m = aggregate(data[, "RMSE"], by=list(dataset=data[,"dataset"], method=data[, "method"], rank=data[, "rank"]), mean)
+agg.s = aggregate(data[, "RMSE"], by=list(dataset=data[,"dataset"], method=data[, "method"], rank=data[, "rank"]), sd)
+
+# Aggregate mean and sd per rank
+inxs = agg.m$method == "L2KRR"
+t = agg.m[inxs, "x"] + agg.s[inxs, "x"]  
+names(t) <- agg.m[inxs, "dataset"]
+
+# Select valid entries
+filt = agg.m$x < t[agg.m$dataset] & agg.m$method %in% meths
+agg.valid = agg.m[filt,]
+agg.min = aggregate(agg.valid$rank, by=list(dataset=agg.valid$dataset, method=agg.valid$method), min) 
+
+for (i in 1:nrow(agg.min))
+R[agg.min[i, "dataset"], agg.min[i, "method"]] = agg.min[i, "x"]
+
+# Rank
+R[is.na(R)] = 85
+rnk = apply(R, MARGIN = 1, FUN=rank)
+rm = sort(rowMeans(rnk))
+print(rm)
+
+# Store CD
+fname = sprintf("../output/delve_regression/rank_CD_%d.pdf", p)
+pdf(fname)
+plotCD(-R, alpha = 0.05)
+dev.off()
+message(sprintf("Written %s", fname))
