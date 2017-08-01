b2499882103fae9d20ef17bb0097d4cf2b6550b0
mstrazar <martin.strazar@gmail.com>
2017-08-01 16:34:11 +0200

    Add timing info.

--- examples/delve/delve_regression2_rank.R
+++ examples/delve/delve_regression2_rank.R
@@ -2,16 +2,15 @@ require(scmamp)
 require(xtable)
 setwd("/Users/martin/Dev/mklaren/examples/delve/")
 
-# Relevant files
-in_dir = "2017-7-28"
-in_files = Sys.glob(sprintf(file.path("../output/delve_regression/", in_dir, "/results_*.csv")))
+# Relevant files - KEEL
+# name = "keel"
+# in_dir = "2017-7-28"
+# in_files = Sys.glob(sprintf(file.path("../output/delve_regression/", in_dir, "/results_*.csv")))
 
-# Aggregate results
-meths = c("CSI", "ICD", "Mklaren", "Nystrom",  "RFF", "FITC")
-datasets = basename(Sys.glob("/Users/martin/Dev/mklaren/datasets/keel/*"))
-R = matrix(NA, nrow=length(datasets), ncol=length(meths))
-row.names(R) = datasets
-colnames(R) = meths
+# Relevant files - DELVE
+name = "delve"
+in_dir = "2017-7-31"
+in_files = Sys.glob(sprintf(file.path("../output/delve_regression/", in_dir, "/results_*.csv")))
 
 # Bind rows
 alldata = data.frame()
@@ -20,23 +19,36 @@ for (in_file in in_files){
   alldata = rbind(alldata, data)
 }
 
+# Aggregate results
+meths = c("CSI", "ICD", "Mklaren", "Nystrom",  "RFF", "FITC")
+datasets = unique(alldata$dataset)
+R = matrix(NA, nrow=length(datasets), ncol=length(meths))
+row.names(R) = datasets
+colnames(R) = meths
+
+# Store times
+Time = matrix(NA, nrow=length(datasets), ncol=length(meths))
+row.names(Time) = datasets
+colnames(Time) = meths
+
+
 # Input checks 
 alldata = alldata[alldata$method == "L2KRR" | (alldata$method %in% meths),]
 alldata$RMSE_tr[is.na(alldata$RMSE_tr)] = Inf
 alldata$RMSE_va[is.na(alldata$RMSE_va)] = Inf
 alldata$RMSE[is.na(alldata$RMSE)] = Inf
 
-# Select lambda via cross-validation
-agg = aggregate(alldata$RMSE_va, by=list(dataset=alldata$dataset,
-                                         method=alldata$method,
-                                         rank=alldata$rank,
-                                         iteration=alldata$iteration,
-                                         p=alldata$p), min)
-row.names(agg) <- sprintf("%s.%s.%d.%s.%d", agg$dataset, agg$method, agg$rank, agg$iteration, agg$p)
-inxs = sprintf("%s.%s.%d.%s.%d", alldata$dataset, alldata$method, 
-               alldata$rank, alldata$iteration, alldata$p)
-alldata$best = alldata$RMSE_va == agg[inxs, "x"]
-data = alldata[alldata$best,]
+  # Select lambda via cross-validation
+  agg = aggregate(alldata$RMSE_va, by=list(dataset=alldata$dataset,
+                                           method=alldata$method,
+                                           rank=alldata$rank,
+                                           iteration=alldata$iteration,
+                                           p=alldata$p), min)
+  row.names(agg) <- sprintf("%s.%s.%d.%s.%d", agg$dataset, agg$method, agg$rank, agg$iteration, agg$p)
+  inxs = sprintf("%s.%s.%d.%s.%d", alldata$dataset, alldata$method, 
+                 alldata$rank, alldata$iteration, alldata$p)
+  alldata$best = alldata$RMSE_va == agg[inxs, "x"]
+  data = alldata[alldata$best,]
 
 # Aggregate mean and sd per rank
 agg.m = aggregate(data[, "RMSE"], by=list(dataset=data[,"dataset"], method=data[, "method"], rank=data[, "rank"]), mean)
@@ -52,8 +64,15 @@ filt = agg.m$x < t[agg.m$dataset] & agg.m$method %in% meths
 agg.valid = agg.m[filt,]
 agg.min = aggregate(agg.valid$rank, by=list(dataset=agg.valid$dataset, method=agg.valid$method), min) 
 
-for (i in 1:nrow(agg.min))
-R[agg.min[i, "dataset"], agg.min[i, "method"]] = agg.min[i, "x"]
+# Extract minimal rank and corresponding time
+for (i in 1:nrow(agg.min)){
+  R[agg.min[i, "dataset"], agg.min[i, "method"]] = agg.min[i, "x"]
+  t = mean(-data[data$dataset == agg.min[i, "dataset"] & 
+           data$rank == agg.min[i, "x"] & 
+           data$method == agg.min[i, "method"], "time"])
+  Time[agg.min[i, "dataset"], agg.min[i, "method"]] = t
+}
+Time[is.na(Time)] = max(Time, na.rm=TRUE)
 
 # Rank
 R[is.na(R)] = 85
@@ -62,8 +81,15 @@ rm = sort(rowMeans(rnk))
 print(rm)
 
 # Store CD
-fname = sprintf("../output/delve_regression/rank_CD_%d.pdf", p)
+fname = sprintf("../output/delve_regression/rank_CD_%s.pdf", name)
 pdf(fname)
 plotCD(-R, alpha = 0.05)
+dev.off()
+message(sprintf("Written %s", fname))
+
+# Store CD
+fname = sprintf("../output/delve_regression/time_CD_%s.pdf", name)
+pdf(fname)
+plotCD(-Time, alpha = 0.05)
 dev.off()
 message(sprintf("Written %s", fname))
