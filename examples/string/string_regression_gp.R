0987ac871c184a4244bd49be6ccfc3b5cdabdecf
mstrazar <martin.strazar@gmail.com>
2017-08-02 08:54:47 +0200

    Summarizing string_gp experiment.

--- examples/string/string_regression_gp.R
+++ examples/string/string_regression_gp.R
@@ -1,11 +1,11 @@
-require(ggplot)
+require(ggplot2)
 require(scmamp)
 setwd("~/Dev/mklaren/examples/string")
 
 
 # Read data
-# in_file = "../output/string/2017-7-31/results_4.csv"  # Same inducing set at each kernel
-in_file = "../output/string/2017-7-31/results_7.csv"  # Different inducing set at each kernel
+in_file = "../output/string/2017-8-1/results_0.csv"  # Different inducing set at each kernel ; 30 iters ;
+# in_file = "../output/string/2017-8-1/results_1.csv"  # Different inducing set at each kernel N=500; L=500 ; 30 iters ;
 data = read.csv(in_file, stringsAsFactors = FALSE, header = TRUE)
 
 # Cross-validation
@@ -15,7 +15,6 @@ row.names(agg) <- agg$id
 data$best = agg[data$id, "x"] == data$evar_va
 data = data[data$best,]
 
-
 # 1 v 1 matrix
 data$pars = paste(data$iteration, data$p, sep=".")
 pars = unique(data$pars)
@@ -23,5 +22,10 @@ methods = unique(data$method)
 R = matrix(NA, nrow=length(pars), ncol=length(methods))
 colnames(R) <- methods
 row.names(R) <- pars
-for(i in 1:nrow(data)) R[data[i, "pars"], data[i, "method"]] = data[i, "evar"]
+for(i in 1:nrow(data)) R[data[i, "pars"], data[i, "method"]] = round(data[i, "evar"], 2)
+
+# Store CD plot
+fname = "../output/string/CD_rank_evar.pdf"
+pdf(fname)
 plotCD(R)
+dev.off()
