5fb92823e07e5e4997fba4e322e9e24e66901d42
mstrazar <martin.strazar@gmail.com>
2017-08-18 13:39:29 +0200

    Run experiments with sparse matrices.

---
+++ examples/rnacontext/rnacontext_regr_spectrum.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Run blitzer_sentiment for L2KRR; Features are still pre-filtered.
+export PYTHONPATH=../..
+export MKL_NUM_THREADS=3
+export NUMEXPR_NUM_THREADS=3
+export OMP_NUM_THREADS=3
+
+for rank in 10 ; do
+    for ddir in ../../datasets/rnacontext/weak/*.txt.gz ; do
+        dset=`basename $ddir`
+        echo python rnacontext_regr_spectrum.py $dset $rank
+        python rnacontext_regr_spectrum.py $dset $rank 2>$dset.$rank.spec.err.txt 1>$dset.$rank.spec.out.txt &
+        sleep 1
+    done
+done
+
+echo "Waiting..."
+wait
+echo "End."
