1196c558d57ff46f79ac4b74a00de84ab64d786f
mstrazar <martin.strazar@gmail.com>
2017-08-18 13:45:25 +0200

    Update to latest results.

--- examples/rnacontext/rnacontext_regr.R
+++ examples/rnacontext/rnacontext_regr.R
@@ -8,9 +8,8 @@ aucs = c(0.53, 0.65, 0.17, 0.81, 0.70, 0.30, 0.96, 0.69)
 names(aucs) = proteins
 
 # Read files 
-# in_files = sprintf("../output/rnacontext/2017-8-13/results_%d.csv", 0:8)  # rank 2 - all test
-# in_files = sprintf("../output/rnacontext/2017-8-10/results_%d.csv", 9:17) # rank 5 - all test
-in_files = sprintf("../output/rnacontext/2017-8-14/results_%d.csv", 0:8)  # rank 10 - all test
+in_files = sprintf("../output/rnacontext/2017-8-17/results_%d.csv", 0:8)  # set of weak sequences
+
 data = data.frame()
 for (in_file in in_files){
   df = read.csv(in_file, stringsAsFactors = FALSE, header = TRUE)  
@@ -20,9 +19,8 @@ for (in_file in in_files){
 # names(ks) = unique(data$kernels)
 # data$kern = ks[data$kernels]
 data$dataset = unlist(lapply(strsplit(data$dataset, "_"), function(x) x[1]))
-
 # Cross-validation
-data$id = paste(data$dataset, data$method, data$iteration, data$p, data$rank, sep=".")
+data$id = paste(data$dataset, data$method, data$iteration, data$rank, sep=".")
 agg = aggregate(data$evar_va, by=list(id=data$id), max)
 row.names(agg) <- agg$id
 data$best = agg[data$id, "x"] == data$evar_va
@@ -41,6 +39,11 @@ colnames(R) <- methods
 row.names(R) <- datasets
 for(i in 1:nrow(agg.m)) R[agg.m[i, "dataset"], agg.m[i, "method"]] = sqrt(agg.m[i, "x"])
 
+
+# Qplot evar
+fname = sprintf("../output/rnacontext/boxplot_evar_%d.pdf", rank)
+ggsave(fname, width=10, height = 3)
+
 # Store CD plot
 fname = sprintf("../output/rnacontext/CD_rank_evar_rank_%d.pdf", rank)
 pdf(fname)
@@ -51,3 +54,5 @@ dev.off()
 rnk = apply(R, 1, rank) 
 rowSums(rnk == 1)
 
+# Wilcoxon signed rank test
+wilcox.test(R[,c("Mklaren")], R[,c("CSI")], paired = TRUE, alternative = "less")
