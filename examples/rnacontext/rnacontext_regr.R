7839ec816430f7e156663cdaef7558891d64e984
mstrazar <martin.strazar@gmail.com>
2017-08-18 18:13:58 +0200

    Update to latest results.

--- examples/rnacontext/rnacontext_regr.R
+++ examples/rnacontext/rnacontext_regr.R
@@ -8,7 +8,8 @@ aucs = c(0.53, 0.65, 0.17, 0.81, 0.70, 0.30, 0.96, 0.69)
 names(aucs) = proteins
 
 # Read files 
-in_files = sprintf("../output/rnacontext/2017-8-17/results_%d.csv", 0:8)  # set of weak sequences
+# in_files = sprintf("../output/rnacontext/2017-8-17/results_%d.csv", 0:8)  # set of weak sequences; n=1000
+in_files = sprintf("../output/rnacontext/2017-8-18/results_%d.csv", 1:18)  # set of weak sequences; n=3000
 
 data = data.frame()
 for (in_file in in_files){
@@ -18,7 +19,9 @@ for (in_file in in_files){
 # ks = 1:length(unique(data$kernels))
 # names(ks) = unique(data$kernels)
 # data$kern = ks[data$kernels]
-data$dataset = unlist(lapply(strsplit(data$dataset, "_"), function(x) x[1]))
+data$dataset = unlist(lapply(strsplit(data$dataset, "_"), function(x) sprintf("%s.%s", x[1], x[4])))
+data$dataset = gsub(".txt.gz", "", data$dataset)
+
 # Cross-validation
 data$id = paste(data$dataset, data$method, data$iteration, data$rank, sep=".")
 agg = aggregate(data$evar_va, by=list(id=data$id), max)
@@ -42,6 +45,7 @@ for(i in 1:nrow(agg.m)) R[agg.m[i, "dataset"], agg.m[i, "method"]] = sqrt(agg.m[
 
 # Qplot evar
 fname = sprintf("../output/rnacontext/boxplot_evar_%d.pdf", rank)
+qplot(data=data, x=as.factor(dataset), y=evar, geom="boxplot", fill=method, xlab="Dataset", ylab="Expl. var.")
 ggsave(fname, width=10, height = 3)
 
 # Store CD plot
