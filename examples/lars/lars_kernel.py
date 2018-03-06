2c2108e5b7d270a99e8af37834450f3812c445da
mstrazar <martin.strazar@gmail.com>
2018-03-06 17:05:47 +0100

    Simple in-place Cholesky functions.

---
+++ examples/lars/lars_kernel.py
@@ -0,0 +1,81 @@
+import numpy as np
+from mklaren.util.la import safe_divide as div
+from mklaren.mkl.mklaren import ssqrt
+from mklaren.kernel.kinterface import Kinterface
+from mklaren.kernel.kernel import exponential_kernel
+
+
+def cholesky_steps(K, G, D, start, act, ina, order=None, no_steps=None):
+    """
+    Perform Cholesky steps for kernel K, starting from the existing matrix
+    G at index k. Order of newly added pivots may be specified.
+
+    Updates the matrix K in-place;
+
+    :param K: Kernel matrix / interface.
+    :param G: Existing Cholesky factors.
+    :param D: Existing diagonal.
+    :param start: Starting index.
+    :param order: Possible to specify desired order. If not specified, standard gain criterion is taken.
+    :param no_steps. Number of steps to take.
+    :return: Updated Cholesky factors.
+    """
+    if order is None:
+        no_steps = K.shape[0] if no_steps is None else no_steps
+        have_order = False
+    else:
+        no_steps = len(order)
+        have_order = True
+
+    for ki, k in enumerate(xrange(start, start + no_steps)):
+
+        # Select best pivot
+        i = order[ki] if have_order else ina[np.argmax(D[ina])]
+        act.append(i)
+        ina.remove(i)
+
+        # Perform Cholesky step for the selected pivot
+        j = list(ina)
+        G[:, k] = 0
+        G[i, k] = ssqrt(D[i])
+        G[j, k] = div(1.0, G[i, k]) * (K[j, i] - G[j, :k].dot(G[i, :k].T))
+
+        # Store previous and update diagonal
+        D[j] = D[j] - (G[j, k] ** 2)
+        D[i] = 0
+
+
+def cholesky(K, rank=None):
+    """ Wrap cholesky steps to perform a Cholesky decomposition of K."""
+    n = K.shape[0]
+    k = n if rank is None else rank
+    G = np.zeros((n, k))
+    D = K.diag() if isinstance(K, Kinterface) else np.diag(K).copy()
+    cholesky_steps(K, G, D, 0, act=[], ina=range(n), no_steps=k)
+    return G
+
+
+def test_cholesky():
+    """ Simple test for complete Cholesky. """
+    n = 10
+    X = np.random.rand(n, 1)
+    K = exponential_kernel(X, X, gamma=1)
+    G = cholesky(K)
+    norms = np.round(np.array([np.linalg.norm(K[:, :] - G[:, :i+1].dot(G[:, :i+1].T)) for i in range(n)]),
+                     decimals=5)
+    assert all(norms[:-1] >= norms[1:])
+    assert np.linalg.norm(K-G.dot(G.T)) < 1e-5
+
+
+def test_cholesky_interface():
+    """ Simple test for complete Cholesky - Kinterface. """
+    n = 10
+    X = np.random.rand(n, 1)
+    K = Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 1})
+    G = cholesky(K)
+    norms = np.round(np.array([np.linalg.norm(K[:, :] - G[:, :i + 1].dot(G[:, :i + 1].T)) for i in range(n)]),
+                     decimals=5)
+    assert all(norms[:-1] >= norms[1:])
+    assert np.linalg.norm(K[:, :]-G.dot(G.T)) < 1e-5
+
+
