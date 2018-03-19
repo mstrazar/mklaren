fc59cfb37ee96b0d210574dbeb7e79a6acb5c915
mstrazar <martin.strazar@gmail.com>
2018-03-19 11:28:33 +0100

    Draft implementation of MKL-QR with penalty functions.

--- examples/lars/lars_mkl.py
+++ examples/lars/lars_mkl.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.linalg import norm
 from scipy.stats import multivariate_normal as mvn
 from examples.lars.lars_beta import plot_path, plot_residuals
 from examples.lars.cholesky import cholesky_steps
 from examples.lars.qr import qr_steps, qr_reorder, qr_orient
+from examples.lars.lars_group import p_ri, p_const, p_sc
 from warnings import warn
 from mklaren.util.la import safe_divide as div
 from mklaren.kernel.kinterface import Kinterface
 from mklaren.kernel.kernel import exponential_kernel
 
 # TODO: add a custom gain function with penalty
+# TODO: debugging: maximum delta (changes at each step) should yield exact estimates
+# TODO: define costs in terms of ||<X, y>|| and |<z, y>|
 
 hlp = """
     LARS with multiple kernels.
@@ -17,13 +21,8 @@ hlp = """
 """
 
 
-def f(p):
-    """ Penalty function. """
-    return p / (1.0 + p)
-
-
-def mkl_qr(Ks, y, rank, delta, f=f):
-    """ LARS-MKL fitting algorithm with a penalty function. """
+def mkl_qr(Ks, y, rank, delta, f=p_ri):
+    """ LARS-MKL sampling algorithm with a penalty function. Produces a feature space. """
     if delta == 1:
         raise ValueError("Unstable selection of delta. (delta = 1)")
 
@@ -34,15 +33,16 @@ def mkl_qr(Ks, y, rank, delta, f=f):
     Acts = []
     Inas = []
 
-    # Master QR decomposition and index of kernels (Permutation)
+    # Master QR decomposition and index of kernels and Permutation
     Q = np.zeros((n, rank + k * delta))
     R = np.zeros((rank + k * delta, rank + k * delta))
     P = []
 
     # Current status and costs
-    corr = np.zeros((k, ))
-    penalty = np.zeros((k,))
-    gain = np.zeros((k, n))
+    corr = np.zeros((k, ))          # Current correlation per kernel || X.T y || (required for calculating gain)
+    cost = np.zeros((k, ))          # Current derived cost per kernel || X.T y || * f(p)
+    ncol = np.zeros((k,))           # Current number of columns per kernel (p)
+    gain = np.zeros((k, n))         # Individual column gains
 
     # Look-ahead phase
     for j in range(len(Ks)):
@@ -61,11 +61,14 @@ def mkl_qr(Ks, y, rank, delta, f=f):
                 G = Gs[j]
                 L_delta = G.dot(G.T) - G[:, :step].dot(G[:, :step].T)
                 IL = (np.eye(n) - Q[:, :step].dot(Q[:, :step].T)).dot(L_delta)
-                B = np.round(np.linalg.norm(IL, axis=0) ** 2, 5)
+                B = np.round(norm(IL, axis=0) ** 2, 5)
                 C = np.absolute(y.T.dot(IL)) ** 2
-                gain[j, :] = div(C, B).ravel()
+                zy2 = div(C, B).ravel()  # |<z, y>|^2 approx.
+                gain[j, :] = zy2 * f(ncol[j] + 1) + (f(ncol[j] + 1) - f(ncol[j])) * corr[j]
             else:
                 gain[j, :] = Ks[j].diagonal() - (Gs[j] ** 2).sum(axis=1).ravel()
+
+            gain[j, Acts[j]] = -np.inf
 
         # Select optimal kernel and pivot
         kern, pivot = np.unravel_index(np.argmax(gain), gain.shape)
@@ -74,7 +77,6 @@ def mkl_qr(Ks, y, rank, delta, f=f):
             warn(msg)
             rank = step
             break
-        assert len(Acts[kern]) == 0 or np.linalg.norm(gain[kern, Acts[kern]]) == 0
         assert pivot not in Acts[kern]
 
         # Select pivot and update Cholesky factors; try simplyfing
@@ -89,7 +91,7 @@ def mkl_qr(Ks, y, rank, delta, f=f):
         G[:, k_start:] = 0
         cholesky_steps(K, G, start=k_start, act=Acts[kern], ina=Inas[kern], order=[pivot])
         qr_steps(G, Q, R, max_steps=1, start=k_start, qstart=step)
-        assert np.linalg.norm(G[:, :k_num] - (Q.dot(R))[:, k_inx]) < 1e-5
+        assert norm(G[:, :k_num] - (Q.dot(R))[:, k_inx]) < 1e-5
 
         # Clear invalid columns and update lookahead steps;
         # Copy act/ina
@@ -99,14 +101,16 @@ def mkl_qr(Ks, y, rank, delta, f=f):
                        ina=list(set(range(n)) - set(Acts[kern])),
                        start=k_num,
                        max_steps=max_steps)
-        assert np.linalg.norm(G[:, :k_num] - (Q.dot(R))[:, k_inx]) < 1e-5
+        assert norm(G[:, :k_num] - (Q.dot(R))[:, k_inx]) < 1e-5
 
-        # Update current correlation
-        corr[kern] = np.linalg.norm(Q[:, k_inx].T.dot(y))
+        # Update current correlation and total cost
+        ncol[kern] += 1
+        corr[kern] = norm(Q[:, k_inx].T.dot(y)) ** 2
+        cost[kern] = corr[kern] * f(ncol[kern])
 
     # Correct lars order is based on including groups ; Gs are no longer valid
-    # del Gs
-    korder = np.argsort(-corr)
+    del Gs
+    korder = np.argsort(-cost)
     porder = []
     for j in korder:
         porder.extend(list(np.where(np.array(P) == j)[0]))
@@ -121,9 +125,9 @@ def mkl_qr(Ks, y, rank, delta, f=f):
     return Q, R, P
 
 
-def mkl_lars(Q, P, y):
+def mkl_lars(Q, P, y, f=p_ri):
     """ Compute the group LARS path. """
-    korder = list(set(P))
+    korder = sorted(set(P), key=lambda p: np.argmax(P == p))
     pairs = zip(korder, korder[1:])
     path = np.zeros((len(korder), Q.shape[1]))
     t = np.sum(P == korder[0])
@@ -132,10 +136,13 @@ def mkl_lars(Q, P, y):
 
     # Compute steps
     for i, (k1, k2) in enumerate(pairs):
-        c1 = np.linalg.norm(Q[:, P == k1].T.dot(r))
-        c2 = np.linalg.norm(Q[:, P == k2].T.dot(r))
+        p1 = sum(P == k1)
+        p2 = sum(P == k2)
+        c1 = norm(Q[:, P == k1].T.dot(r))**2 * f(p1)
+        c2 = norm(Q[:, P == k2].T.dot(r))**2 * f(p2)
+        print(c1, c2)
         assert c2 <= c1
-        alpha = 1 - (c2 / c1)
+        alpha = 1 - np.sqrt(c2 / c1)
         path[i] = alpha * Q.T.dot(r).ravel()
         t += np.sum(P == k2)
         r = r - Q[:, :t].dot(path[i])
@@ -145,7 +152,7 @@ def mkl_lars(Q, P, y):
     path[-1] = Q.T.dot(r).ravel()
     r = r - Q.dot(path[-1])
     mu = mu + Q.dot(path[-1])
-    assert np.linalg.norm(Q.T.dot(r)) < 1e-3
+    assert norm(Q.T.dot(r)) < 1e-3
     return path, mu
 
 
@@ -164,7 +171,7 @@ def test_lars_mkl():
         Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 0.6})[:, :], # short
         Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 0.1})[:, :], # long
         ]
-    Kt = 1.0 + Ks[0] + 0.01 * Ks[1]
+    Kt = 1.0 + Ks[0] + 0.0 * Ks[1]
     y = mvn.rvs(mean=np.zeros(n,), cov=Kt).reshape((n, 1))
 
     # Fit model
@@ -181,3 +188,37 @@ def test_lars_mkl():
     # Plot path
     plot_path(path)
     plot_residuals(Q, y, path)
+
+
+# Unit tests
+def compare_penalty():
+    """ Simple test for LARS-kernel. """
+    noise = 0.5
+    n = 100
+    rank = 10
+    delta = 5
+    X = np.linspace(-10, 10, n).reshape((n, 1))
+    Ks = [
+        Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 1.0})[:, :],    # short
+        Kinterface(data=X, kernel=exponential_kernel, kernel_args={"gamma": 0.1})[:, :],    # long
+        ]
+    Kt = 1.0 + Ks[0] + 1.0 * Ks[1] + noise * np.eye(n)
+    y = mvn.rvs(mean=np.zeros(n,), cov=Kt).reshape((n, 1))
+
+    # Fit models
+    Q_ri, R_ri, P_ri = mkl_qr(Ks, y, rank, delta, f=p_ri)
+    path_ri, mu_ri = mkl_lars(Q_ri, P_ri, y, p_ri)
+
+    Q_co, R_co, P_co = mkl_qr(Ks, y, rank, delta, f=p_const)
+    path_co, mu_co = mkl_lars(Q_co, P_co, y, p_const)
+
+    Q_sc, R_sc, P_sc = mkl_qr(Ks, y, rank, delta, f=p_sc)
+    path_sc, mu_sc = mkl_lars(Q_sc, P_sc, y, p_sc)
+
+    plt.figure()
+    plt.plot(y, ".")
+    plt.plot(mu_co, "-", label="unscaled", color="gray")
+    plt.plot(mu_sc, "-", label="scaled", color="pink")
+    plt.plot(mu_ri, "-", label="rich", color="green")
+    plt.legend()
+
