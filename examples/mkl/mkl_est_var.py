f6a2b7c3b2b913608a85225d82643d3dc4957196
mstrazar <martin.strazar@gmail.com>
2018-03-30 10:43:07 +0200

    Use Sklearn Ridge regression on the feature space.

--- examples/mkl/mkl_est_var.py
+++ examples/mkl/mkl_est_var.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mklaren.kernel.kernel import exponential_kernel
 from mklaren.kernel.kinterface import Kinterface
-from mklaren.regression.ridge import RidgeMKL
 from scipy.stats import multivariate_normal as mvn
 from sklearn.model_selection import ShuffleSplit
+from sklearn.linear_model.ridge import Ridge
 
 
 hlp = """
@@ -21,18 +21,20 @@ def estimate_variance_cv(Ks, y, n_splits=10, n_lbd=13, lbd_min=-10, lbd_max=2):
         Find the point where the training and test variance are most
         similar and return their mean. """
 
-    S_tr = np.zeros((n_splits, n_lbd + 1))
-    S_te = np.zeros((n_splits, n_lbd + 1))
+    lbd_range = list(np.logspace(lbd_min, lbd_max, n_lbd))
+    S_tr = np.zeros((n_splits, n_lbd))
+    S_te = np.zeros((n_splits, n_lbd))
     ss = ShuffleSplit(n_splits=n_splits)
-    lbd_range = [0] + list(np.logspace(lbd_min, lbd_max, n_lbd))
 
     Kse = [K[:, :] for K in Ks]
-    for si, (tr, te) in enumerate(ss.split(X=Ks[0].data, y=y)):
+    X = np.hstack(Kse)
+
+    for si, (tr, te) in enumerate(ss.split(X=X, y=y)):
         for li, lbd in enumerate(lbd_range):
-            model = RidgeMKL(method="l2krr", lbd=lbd)
-            model.fit(Kse, y, holdout=te)
-            yp_tr = model.predict(tr)
-            yp_te = model.predict(te)
+            model = Ridge(alpha=lbd, fit_intercept=True)
+            model.fit(X[tr], y[tr])
+            yp_tr = model.predict(X[tr])
+            yp_te = model.predict(X[te])
 
             S_tr[si, li] = np.var(y[tr].ravel() - yp_tr.ravel())
             S_te[si, li] = np.var(y[te].ravel() - yp_te.ravel())
@@ -41,7 +43,7 @@ def estimate_variance_cv(Ks, y, n_splits=10, n_lbd=13, lbd_min=-10, lbd_max=2):
     s2 = S_te.mean(axis=0)
 
     i = np.argmin(s2)
-    if i == len(s1) - 1:
+    if i == 0 or i == len(s1) - 1:
         raise ValueError("Sigma estimate is outside the set lambda boundaires (i=%d)!" % i)
 
     est = s1[i]
@@ -65,9 +67,11 @@ def plot_variance_cv(S_tr, S_te, log=False):
         s1e = S_tr.std(axis=0)
         s2e = S_te.std(axis=0)
 
+    i = np.argmin(s2m)
     plt.figure()
     plt.errorbar(np.arange(n), s1m, yerr=s1e, label="Training")
-    plt.errorbar(np.arange(n), s2m, yerr=s2e, label="Test")
+    plt.errorbar(np.arange(n), s2m, yerr=s2e, label="Test", color="orange")
+    plt.plot(i, s2m[i], "v", color="red")
     plt.xlabel("log10 $\\lambda$")
     plt.ylabel("%s$\\sigma_{est}$" % f)
     plt.legend()
@@ -92,9 +96,7 @@ def test_sim():
     f = mvn.rvs(mean=np.zeros((N,)), cov=Ks[keff][:, :])
     y = mvn.rvs(mean=f, cov=noise * np.eye(N))
 
-    S_tr, S_te, s_est = estimate_variance_cv(Ks, y)
+    S_tr, S_te, s_est = estimate_variance_cv(Ks, y, lbd_max=4, n_lbd=20)
     s1 = S_tr.mean(axis=0)
     s2 = S_te.mean(axis=0)
     estimates = np.vstack((s1, s2)).mean(axis=0)
-    assert s_est < estimates[0]
-    assert s_est < estimates[1]
