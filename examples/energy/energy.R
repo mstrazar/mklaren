d4eea24e27b991fc76a85eeff8b9927bf68d79b0
mstrazar <martin.strazar@gmail.com>
2018-04-30 10:44:53 +0200

    Process only available methods.

--- examples/energy/energy.R
+++ examples/energy/energy.R
@@ -17,8 +17,9 @@ out_dir = opt$output
 dir.create(out_dir, showWarnings = FALSE)
 
 # Selection of methods
-methods = c("Mklaren", "CSI", "ICD", "Nystrom", "RFF", "RFF-NS", "SPGP")
 alldata = read.csv(in_file, header = TRUE, stringsAsFactors = FALSE) 
+methods = unique(alldata$method)
+
 
 # Select scores via cross-validation
 alldata$name = sprintf("%s.%s.%s.%s", alldata$method, alldata$tsi, 
